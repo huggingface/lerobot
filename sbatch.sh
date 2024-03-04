570f8d01dff4bae788c8fc3952518b0995f0fe70
Remi Cadene <re.cadene@gmail.com>
2024-03-04 22:41:47 +0000

    Revert "WIP"
    This reverts commit 20c08bb74091388a7f5a55866ed39b151fd1b95d.

--- sbatch.sh
+++ sbatch.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 #SBATCH --nodes=1            # total number of nodes (N to be defined)
 #SBATCH --ntasks-per-node=1  # number of tasks per node (here 8 tasks, or 1 task per GPU)
-#SBATCH --qos=normal         # number of GPUs reserved per node (here 8, or all the GPUs)
-#SBATCH --partition=hopper-prod
 #SBATCH --gres=gpu:1         # number of GPUs reserved per node (here 8, or all the GPUs)
-#SBATCH --cpus-per-task=12    # number of cores per task
-#SBATCH --mem-per-cpu=11G
-#SBATCH --time=12:00:00
-#SBATCH --output=/admin/home/remi_cadene/slurm/%j.out
-#SBATCH --error=/admin/home/remi_cadene/slurm/%j.err
-#SBATCH --mail-user=remi_cadene@huggingface.co
+#SBATCH --cpus-per-task=8    # number of cores per task (8x8 = 64 cores, or all the cores)
+#SBATCH --time=2-00:00:00
+#SBATCH --output=/home/rcadene/slurm/%j.out
+#SBATCH --error=/home/rcadene/slurm/%j.err
+#SBATCH --qos=medium
+#SBATCH --mail-user=re.cadene@gmail.com
 #SBATCH --mail-type=ALL
 
 CMD=$@
 echo "command: $CMD"
 
-# apptainer exec --nv \
-# ~/apptainer/nvidia_cuda:12.2.2-devel-ubuntu22.04.sif $SHELL
+apptainer exec --nv \
+~/apptainer/nvidia_cuda:12.2.2-devel-ubuntu22.04.sif $SHELL
 
-#source ~/.bashrc
-#conda activate fowm
+source ~/.bashrc
+conda activate fowm
 
 srun $CMD
