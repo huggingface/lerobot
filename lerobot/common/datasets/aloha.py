10034e85c4df5411c60940373b75cb4ae565282b
Cadene <re.cadene@gmail.com>
2024-03-19 16:03:42 +0000

    Aloha done

--- lerobot/common/datasets/aloha.py
+++ lerobot/common/datasets/aloha.py
@@ -84,7 +84,7 @@ class AlohaExperienceReplay(AbstractExperienceReplay):
     def __init__(
         self,
         dataset_id: str,
-        version: str | None = None,
+        version: str | None = "v1.0",
         batch_size: int = None,
         *,
         shuffle: bool = True,
