ff7239c764c860ff3cb650b57d7ed35b52cf11fc
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2024-04-25 17:37:51 +0200

    style

--- lerobot/scripts/train.py
+++ lerobot/scripts/train.py
@@ -300,8 +300,6 @@ def train(cfg: dict, out_dir=None, job_name=None):
     elif policy.name == "tdmpc":
         raise NotImplementedError("TD-MPC not implemented yet.")
 
-
-
     num_learnable_params = sum(p.numel() for p in policy.parameters() if p.requires_grad)
     num_total_params = sum(p.numel() for p in policy.parameters())
 
