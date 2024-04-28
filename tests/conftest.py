d12d6e80e8fcf96091181c05897434f2d9ff1a84
Simon Alibert <alibert.sim@gmail.com>
2024-04-28 15:13:55 +0200

    Add --disable-data-loader-warning

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,23 @@
 from .utils import DEVICE
 
 
-def pytest_collection_finish():
+def pytest_addoption(parser):
+    parser.addoption(
+        "--disable-data-loader-warning", action="store_true", help="Silence the DataLoader worker warning"
+    )
+
+
+def pytest_configure(config):
+    config.disable_data_loader_warning = config.getoption("--disable-data-loader-warning")
+
+
+def pytest_collection_finish(session):
     print(f"\nTesting with {DEVICE=}")
+    if session.config.disable_data_loader_warning:
+        import warnings
+
+        warnings.filterwarnings(
+            "ignore",
+            message="This DataLoader will create .* worker processes in total. Our suggested max number of worker in current system is .*",
+            category=UserWarning,
+        )
