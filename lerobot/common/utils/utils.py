135ba53f7615cf06ed3c3bf77c911cda408f1ade
Simon Alibert <alibert.sim@gmail.com>
2024-04-28 18:31:05 +0200

    Upgrade version_base to 1.2

--- lerobot/common/utils/utils.py
+++ lerobot/common/utils/utils.py
@@ -93,7 +93,7 @@ def init_hydra_config(config_path: str, overrides: list[str] | None = None) -> D
     # Hydra needs a path relative to this file.
     hydra.initialize(
         str(_relative_path_between(Path(config_path).absolute().parent, Path(__file__).absolute().parent)),
-        version_base="1.1",
+        version_base="1.2",
     )
     cfg = hydra.compose(Path(config_path).stem, overrides)
     return cfg
