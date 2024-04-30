973d20cdbc3f4f2949e7f4f7ab2a53a55fe6dc85
Cadene <re.cadene@gmail.com>
2024-04-30 11:15:30 +0000

    Refactor

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -242,8 +242,8 @@ def test_flatten_unflatten_dict():
 @pytest.mark.parametrize(
     "repo_id",
     [
-        # "lerobot/pusht",
-        # "lerobot/aloha_sim_insertion_human",
+        "lerobot/pusht",
+        "lerobot/aloha_sim_insertion_human",
         "lerobot/xarm_lift_medium",
     ],
 )
@@ -288,17 +288,17 @@ def test_backward_compatibility(repo_id):
     # TODO(rcadene): Enable testing on second and last episode
     # We currently cant because our test dataset only contains the first episode
 
-    # test 2 first frames of second episode
-    i = dataset.episode_data_index["from"][1].item()
-    load_and_compare(i)
-    load_and_compare(i + 1)
+    # # test 2 first frames of second episode
+    # i = dataset.episode_data_index["from"][1].item()
+    # load_and_compare(i)
+    # load_and_compare(i + 1)
 
-    # test 2 last frames of second episode
-    i = dataset.episode_data_index["to"][1].item()
-    load_and_compare(i - 2)
-    load_and_compare(i - 1)
+    # # test 2 last frames of second episode
+    # i = dataset.episode_data_index["to"][1].item()
+    # load_and_compare(i - 2)
+    # load_and_compare(i - 1)
 
-    # test 2 last frames of last episode
-    i = dataset.episode_data_index["to"][-1].item()
-    load_and_compare(i - 2)
-    load_and_compare(i - 1)
+    # # test 2 last frames of last episode
+    # i = dataset.episode_data_index["to"][-1].item()
+    # load_and_compare(i - 2)
+    # load_and_compare(i - 1)
