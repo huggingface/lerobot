ce89d91e321acbe568b52ce08dc1bdb9b6dfee92
Simon Alibert <alibert.sim@gmail.com>
2024-05-11 10:17:19 +0200

    Add policy-specific copyrights

--- lerobot/common/policies/diffusion/modeling_diffusion.py
+++ lerobot/common/policies/diffusion/modeling_diffusion.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-# Copyright 2024 The HuggingFace Inc. team. All rights reserved.
+# Copyright 2024 Columbia Artificial Intelligence, Robotics Lab,
+# and The HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
