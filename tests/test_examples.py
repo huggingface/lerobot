e67da1d7a665622c89d32cd2a58e3b4cc5fd6f4a
Alexander Soare <alexander.soare159@gmail.com>
2024-05-21 16:47:49 +0100

    Add tutorials for using the training script and  (#196)
    Co-authored-by: Remi <re.cadene@gmail.com>

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -45,11 +45,11 @@ def test_example_1():
 
 
 @require_package("gym_pusht")
-def test_examples_2_through_4():
+def test_examples_basic2_basic3_advanced1():
     """
     Train a model with example 3, check the outputs.
     Evaluate the trained model with example 2, check the outputs.
-    Calculate the validation loss with example 4, check the outputs.
+    Calculate the validation loss with advanced example 1, check the outputs.
     """
 
     ### Test example 3
@@ -97,7 +97,7 @@ def test_examples_2_through_4():
     assert Path("outputs/eval/example_pusht_diffusion/rollout.mp4").exists()
 
     ## Test example 4
-    file_contents = _read_file("examples/4_calculate_validation_loss.py")
+    file_contents = _read_file("examples/advanced/2_calculate_validation_loss.py")
 
     # Run on a single example from the last episode, use CPU, and use the local model.
     file_contents = _find_and_replace(
