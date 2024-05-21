5df98c3877e28dfcbe5ccf530478cd9711794459
Simon Alibert <alibert.sim@gmail.com>
2024-05-21 17:23:31 +0200

    refactor

--- gym_dora/gym_dora/env.py
+++ gym_dora/gym_dora/env.py
@@ -1,12 +1,14 @@
+import os
+
 import gymnasium as gym
 import numpy as np
-from dora import Node
-import os
 import pyarrow as pa
+from dora import Node
 
 IMAGE_WIDTH = int(os.getenv("IMAGE_WIDTH", "640"))
 IMAGE_HEIGHT = int(os.getenv("IMAGE_HEIGHT", "480"))
 FPS = int(os.getenv("FPS", "30"))
+
 
 class DoraEnv(gym.Env):
     metadata = {"render_modes": ["rgb_array"], "render_fps": FPS}
@@ -14,53 +16,50 @@ class DoraEnv(gym.Env):
     def __init__(self, model="aloha"):
         # Initialize a new node
         self.node = Node()
-        self.observation = {"pixels": {}, "terminated": False}
+        self.observation = {"pixels": {}, "agent_pos": None}
+        self.terminated = False
 
-    def _update(self) -> dict: 
+    def _get_obs(self):
         while True:
             event = self.node.next(timeout=0.001)
 
             ## If event is None, the node event stream is closed and we should terminate the env
             if event is None:
-                self.observation["terminated"] = True
+                self.terminated = True
                 break
 
             if event["type"] == "INPUT":
                 # Map Image input into pixels key within Aloha environment
                 if "cam" in event["id"]:
-                    self.observation["pixels"][event["id"]] = event["value"].to_numpy().reshape(IMAGE_HEIGHT, IMAGE_WIDTH, 3)
+                    self.observation["pixels"][event["id"]] = (
+                        event["value"].to_numpy().reshape(IMAGE_HEIGHT, IMAGE_WIDTH, 3)
+                    )
                 else:
                     # Map other inputs into the observation dictionary using the event id as key
                     self.observation[event["id"]] = event["value"].to_numpy()
-            
+
             # If the event is a timeout error break the update loop.
             elif event["type"] == "ERROR":
                 break
-        
 
     def reset(self, seed: int | None = None):
+        self.node.send_output("reset")
+        self._get_obs()
+        self.terminated = False
+        info = {}
+        return self.observation, info
 
-        self._update()
-        
+    def step(self, action: np.ndarray):
+        # Send the action to the dataflow as action key.
+        self.node.send_output("action", pa.array(action))
+        self._get_obs()
         reward = 0
-        terminated = truncated = self.observation["terminated"]
+        terminated = truncated = self.terminated
         info = {}
         return self.observation, reward, terminated, truncated, info
 
     def render(self): ...
 
-    def step(self, action: np.ndarray):
-        self._update()
-
-        # Send the action to the dataflow as action key.
-        self.node.send_output("action", pa.array(action))
-        reward = 0
-        terminated = truncated = self.observation["terminated"]
-        info = {}
-        return self.observation, reward, terminated, truncated, info
-
     def close(self):
-
         # Drop the node
         del self.node
-
