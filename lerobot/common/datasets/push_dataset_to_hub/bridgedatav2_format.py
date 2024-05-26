759de11e2911036fd66d09f692b4485be15c779a
husainhz7 <husainhz7@gmail.com>
2024-05-26 11:12:44 +0530

    Adds load_from_raw
    Collects images from scripted_raw directory and parses the action and observation dictionaries. Testing with Video fails

--- lerobot/common/datasets/push_dataset_to_hub/bridgedatav2_format.py
+++ lerobot/common/datasets/push_dataset_to_hub/bridgedatav2_format.py
@@ -15,9 +15,18 @@
 # limitations under the License.
 """Process BridgeData V2 files"""
 
+import gc
 import os
 import pickle
+import shutil
 from pathlib import Path
+
+import torch
+import tqdm
+from PIL import Image as PILImage
+
+from lerobot.common.datasets.push_dataset_to_hub.utils import concatenate_episodes, save_images_concurrently
+from lerobot.common.datasets.video_utils import encode_video_frames
 
 
 def check_format(raw_dir) -> bool:
@@ -49,6 +58,76 @@ def load_state(path):
     return x["full_state"]
 
 
+def load_from_raw(raw_dir, out_dir, fps, video, debug):
+    traj_search_path = os.path.join("**", "traj_group*", "traj*")
+    traj_folders = list(raw_dir.glob(traj_search_path))
+    ep_dicts = []
+    episode_data_index = {"from": [], "to": []}
+
+    id_from = 0
+    for ep_idx, ep_path in tqdm.tqdm(enumerate(traj_folders), total=len(traj_folders)):
+        if len(list(ep_path.glob("*.pkl"))) != 2:
+            continue
+
+        image_paths = list(ep_path.glob(os.path.join("images0", "*.jpg")))
+        num_frames = len(image_paths)
+
+        done = torch.zeros(num_frames, dtype=torch.bool)
+        done[-1] = True
+
+        actions = load_actions(ep_path)
+        state = load_state(ep_path)
+        ep_dict = {}
+        img_key = "observation.image"
+        imgs_array = [PILImage.open(x) for x in image_paths]
+
+        if video:
+            # save png images in temporary directory
+            tmp_imgs_dir = out_dir / "tmp_images"
+            save_images_concurrently(imgs_array, tmp_imgs_dir)  # TODO: this isn't working
+
+            # encode images to a mp4 video
+            fname = f"{img_key}_episode_{ep_idx:06d}.mp4"
+            video_path = out_dir / "videos" / fname
+            encode_video_frames(tmp_imgs_dir, video_path, fps)
+
+            # clean temporary images directory
+            shutil.rmtree(tmp_imgs_dir)
+
+            # store the reference to the video frame
+            ep_dict[img_key] = [{"path": f"videos/{fname}", "timestamp": i / fps} for i in range(num_frames)]
+        else:
+            ep_dict[img_key] = imgs_array
+
+        ep_dict["action"] = actions
+        ep_dict["observation.state"] = state
+        ep_dict["episode_index"] = torch.tensor([ep_idx] * num_frames)
+        ep_dict["frame_index"] = torch.arange(0, num_frames, 1)
+        ep_dict["timestamp"] = torch.arange(0, num_frames, 1) / fps
+        ep_dict["next.done"] = done
+
+        assert isinstance(ep_idx, int)
+        ep_dicts.append(ep_dict)
+
+        episode_data_index["from"].append(id_from)
+        episode_data_index["to"].append(id_from + num_frames)
+
+        id_from += num_frames
+
+        gc.collect()
+
+        # process first episode only
+        if debug:
+            break
+    data_dict = concatenate_episodes(ep_dicts)
+    return data_dict, episode_data_index
+
+
 def from_raw_to_lerobot_format(raw_dir: Path, out_dir: Path, fps=None, video=True, debug=False):
     # sanity check
     check_format(raw_dir)
+
+    if fps is None:
+        fps = 12
+
+    data_dir, episode_data_index = load_from_raw(raw_dir, out_dir, fps, video, debug)
