3326cf27f2a8a14fe7d800ddf5cf7ed99612aa47
husainhz7 <husainhz7@gmail.com>
2024-06-01 23:09:48 +0530

    Adds to_hf_dataset Converts raw data to hugging face lerobot format. Edited other functions to account for latencies

--- lerobot/common/datasets/push_dataset_to_hub/bridgedatav2_format.py
+++ lerobot/common/datasets/push_dataset_to_hub/bridgedatav2_format.py
@@ -19,27 +19,48 @@ import gc
 import os
 import pickle
 import shutil
+from datetime import datetime
 from pathlib import Path
 
 import torch
 import tqdm
+from datasets import Dataset, Features, Image, Sequence, Value
 from PIL import Image as PILImage
 
 from lerobot.common.datasets.push_dataset_to_hub.utils import concatenate_episodes, save_images_concurrently
-from lerobot.common.datasets.video_utils import encode_video_frames
+from lerobot.common.datasets.utils import (
+    hf_transform_to_torch,
+)
+from lerobot.common.datasets.video_utils import VideoFrame, encode_video_frames
 
 
 def check_format(raw_dir) -> bool:
     traj_search_path = os.path.join("**", "traj_group*", "traj*")
     traj_folders = list(raw_dir.glob(traj_search_path))
+
     assert len(traj_folders) != 0
     for traj in traj_folders:
-        if len(list(traj.glob("*.pkl"))) == 2:
+        # Data collected prior to 7-23 has a delay of 1, otherwise a delay of 0
+        date_time = datetime.strptime(str(traj).split("/")[-4], "%Y-%m-%d_%H-%M-%S")
+        latency_shift = date_time < datetime(2021, 7, 23)
+        is_in_scripted_raw = "scripted_raw" in str(traj)
+        if len(list(traj.glob("*.pkl"))) == 2:  # ~30 trajectories dont have both files
             actions = load_actions(traj)
             num_frames = len(list(traj.glob(os.path.join("images0", "*.jpg"))))
-            assert len(actions) == num_frames - 1
-            state = load_state(traj)
-            assert len(state) == num_frames
+            state, time_stamp = load_state_timestamp(traj)
+            if is_in_scripted_raw:
+                assert len(actions) == num_frames - 1
+                assert len(state) == num_frames
+                assert len(time_stamp) == num_frames
+            else:
+                if latency_shift:
+                    assert len(actions) == num_frames - 1
+                    assert len(state) == num_frames - 1
+                    assert len(time_stamp) == num_frames - 1
+                else:
+                    assert len(actions) == num_frames - 1
+                    assert len(state) == num_frames
+                    assert len(time_stamp) == num_frames
 
 
 def load_actions(path):
@@ -51,11 +72,11 @@ def load_actions(path):
     return act_list
 
 
-def load_state(path):
+def load_state_timestamp(path):
     fp = os.path.join(path, "obs_dict.pkl")
     with open(fp, "rb") as f:
         x = pickle.load(f)
-    return x["full_state"]
+    return x["full_state"], x["time_stamp"]
 
 
 def load_from_raw(raw_dir, out_dir, fps, video, debug):
@@ -69,6 +90,12 @@ def load_from_raw(raw_dir, out_dir, fps, video, debug):
         if len(list(ep_path.glob("*.pkl"))) != 2:
             continue
 
+        is_in_scripted_raw = "scripted_raw" in str(ep_path)
+
+        if not is_in_scripted_raw:
+            date_time = datetime.strptime(str(ep_path).split("/")[-4], "%Y-%m-%d_%H-%M-%S")
+            latency_shift = date_time < datetime(2021, 7, 23)
+
         image_paths = list(ep_path.glob(os.path.join("images0", "*.jpg")))
         num_frames = len(image_paths)
 
@@ -76,10 +103,18 @@ def load_from_raw(raw_dir, out_dir, fps, video, debug):
         done[-1] = True
 
         actions = load_actions(ep_path)
-        state = load_state(ep_path)
+        state, time_stamp = load_state_timestamp(ep_path)
         ep_dict = {}
         img_key = "observation.image"
         imgs_array = [PILImage.open(x) for x in image_paths]
+
+        if is_in_scripted_raw:
+            actions = [actions[0]] + actions  # duplicate first action to compensate for extra frame
+        else:
+            if latency_shift:
+                state = state[1:]
+                actions = actions[1:]
+                time_stamp = time_stamp[1:]
 
         if video:
             # save png images in temporary directory
@@ -103,8 +138,12 @@ def load_from_raw(raw_dir, out_dir, fps, video, debug):
         ep_dict["observation.state"] = state
         ep_dict["episode_index"] = torch.tensor([ep_idx] * num_frames)
         ep_dict["frame_index"] = torch.arange(0, num_frames, 1)
-        ep_dict["timestamp"] = torch.arange(0, num_frames, 1) / fps
+        ep_dict["timestamp"] = torch.tensor(time_stamp)
         ep_dict["next.done"] = done
+
+        if len(actions) != len(state):
+            print(str(len(actions)) + " " + str(len(state)) + " " + str(num_frames))
+            print(ep_path)
 
         assert isinstance(ep_idx, int)
         ep_dicts.append(ep_dict)
@@ -123,11 +162,38 @@ def load_from_raw(raw_dir, out_dir, fps, video, debug):
     return data_dict, episode_data_index
 
 
+def to_hf_dataset(data_dict, video) -> Dataset:
+    features = {}
+    if video:
+        features["observation.image"] = VideoFrame()
+    else:
+        features["observation.image"] = Image()
+    features["observation.state"] = Sequence(
+        length=len(data_dict["observation.state"][0]), feature=Value(dtype="float32", id=None)
+    )
+    features["action"] = Sequence(length=len(data_dict["action"][0]), feature=Value(dtype="float32", id=None))
+    features["episode_index"] = Value(dtype="int64", id=None)
+    features["frame_index"] = Value(dtype="int64", id=None)
+    features["timestamp"] = Value(dtype="float32", id=None)
+    features["next.done"] = Value(dtype="bool", id=None)
+    features["index"] = Value(dtype="int64", id=None)
+
+    hf_dataset = Dataset.from_dict(data_dict, features=Features(features))
+    hf_dataset.set_transform(hf_transform_to_torch)
+    return hf_dataset
+
+
 def from_raw_to_lerobot_format(raw_dir: Path, out_dir: Path, fps=None, video=True, debug=False):
     # sanity check
     check_format(raw_dir)
 
     if fps is None:
-        fps = 12
+        fps = 30
 
     data_dir, episode_data_index = load_from_raw(raw_dir, out_dir, fps, video, debug)
+    hf_dataset = to_hf_dataset(data_dir, video)
+    info = {
+        "fps": fps,
+        "video": video,
+    }
+    return hf_dataset, episode_data_index, info
