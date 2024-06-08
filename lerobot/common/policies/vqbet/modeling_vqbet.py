670c08a2e48b42d21fb5ce58e3e7ca0560990f88
jayLEE0301 <ysz0301@gmail.com>
2024-06-08 16:38:02 -0400

    minor: change location of comments

--- lerobot/common/policies/vqbet/modeling_vqbet.py
+++ lerobot/common/policies/vqbet/modeling_vqbet.py
@@ -315,11 +315,11 @@ class VQBeTModel(nn.Module):
         # get action features (pass through GPT)
         features = self.policy(input_tokens)
         # len(self.config.input_shapes) is the number of different observation modes. this line gets the index of action prompt tokens.
-        # Behavior Transformer (BeT), and VQ-BeT are both sequence-to-sequence prediction models, mapping sequential observation to sequential action (please refer to section 2.2 in BeT paper https://arxiv.org/pdf/2206.11251).
-        # Thus, it predict historical action sequence, in addition to current and future actions (predicting future actions : optional).
         historical_act_pred_index = np.arange(0, n_obs_steps) * (len(self.config.input_shapes)+1) + len(self.config.input_shapes)
 
-        # only extract the output tokens at the position of action query
+        # only extract the output tokens at the position of action query:
+        # Behavior Transformer (BeT), and VQ-BeT are both sequence-to-sequence prediction models, mapping sequential observation to sequential action (please refer to section 2.2 in BeT paper https://arxiv.org/pdf/2206.11251).
+        # Thus, it predict historical action sequence, in addition to current and future actions (predicting future actions : optional).
         features = torch.cat([
             features[:, historical_act_pred_index],
             features[:, -len_additional_action_token:]
