fd7f65bce35c60ab5702a2bb72e4b113ccdef081
Simon Alibert <alibert.sim@gmail.com>
2024-06-11 09:52:35 +0000

    Refactor example

--- examples/6_add_image_transforms.py
+++ examples/6_add_image_transforms.py
@@ -1,6 +1,7 @@
 """
-This script demonstrates how to implement torchvision image augmentation on an instance of a LeRobotDataset and how to show some transformed images.
-The transformations are passed to the dataset as an argument upon creation, and transforms are applied to the observation images before they are returned.
+This script demonstrates how to use torchvision's image transformation with LeRobotDataset for data
+augmentation purposes. The transformations are passed to the dataset as an argument upon creation, and
+transforms are applied to the observation images before they are returned in the dataset's __get_item__.
 """
 
 from pathlib import Path
@@ -9,26 +10,17 @@ from torchvision.transforms import ToPILImage, v2
 
 from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
 
-to_pil = ToPILImage()
-
-# Create a directory to store output images
-output_dir = Path("outputs/image_transforms")
-output_dir.mkdir(parents=True, exist_ok=True)
-
 dataset_repo_id = "lerobot/aloha_static_tape"
 
 # Create a LeRobotDataset with no transformations
-dataset = LeRobotDataset(dataset_repo_id, image_transforms=None)
+dataset = LeRobotDataset(dataset_repo_id)
+# This is equivalent to `dataset = LeRobotDataset(dataset_repo_id, image_transforms=None)`
 
 # Get the index of the first observation in the first episode
 first_idx = dataset.episode_data_index["from"][0].item()
 
 # Get the frame corresponding to the first camera
 frame = dataset[first_idx][dataset.camera_keys[0]]
-
-# Save the original frame
-to_pil(frame).save(output_dir / "original_frame.png", quality=100)
-print(f"Original frame saved to {output_dir / 'original_frame.png'}.")
 
 
 # Define the transformations
@@ -45,6 +37,15 @@ transformed_dataset = LeRobotDataset(dataset_repo_id, image_transforms=transform
 
 # Get a frame from the transformed dataset
 transformed_frame = transformed_dataset[first_idx][transformed_dataset.camera_keys[0]]
+
+# Create a directory to store output images
+output_dir = Path("outputs/image_transforms")
+output_dir.mkdir(parents=True, exist_ok=True)
+
+# Save the original frame
+to_pil = ToPILImage()
+to_pil(frame).save(output_dir / "original_frame.png", quality=100)
+print(f"Original frame saved to {output_dir / 'original_frame.png'}.")
 
 # Save the transformed frame
 to_pil(transformed_frame).save(output_dir / "transformed_frame.png", quality=100)
