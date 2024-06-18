43b2e265d3b7a6f9ffcd1ecd804d819f7bb320f1
Simon Alibert <75076266+aliberts@users.noreply.github.com>
2024-06-18 20:25:26 +0200

    Add suggestion
    Co-authored-by: Alexander Soare <alexander.soare159@gmail.com>

--- lerobot/common/datasets/_video_benchmark/run_video_benchmark.py
+++ lerobot/common/datasets/_video_benchmark/run_video_benchmark.py
@@ -22,7 +22,7 @@ All of these benchmarks are evaluated within different timestamps modes correspo
     - `1_frame`: 1 single frame is loaded.
     - `2_frames`: 2 consecutive frames are loaded.
     - `2_frames_4_space`: 2 frames separated by 4 frames are loaded.
-    - `6_frames`: 6 frames are loaded.
+    - `6_frames`: 6 consecutive frames are loaded.
 
 These benchmarks are run on the first episode of each dataset specified in DATASET_REPO_IDS.
 Note: These datasets need to be image datasets, not video datasets.
