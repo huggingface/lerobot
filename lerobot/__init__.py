2c101951c2537e81e21d3f6f0c7fcf92539a6fa8
Alexander Soare <alexander.soare159@gmail.com>
2024-07-05 08:10:13 +0100

    fix e2e

--- lerobot/__init__.py
+++ lerobot/__init__.py
@@ -70,7 +70,9 @@ available_datasets_per_env = {
         "lerobot/aloha_sim_transfer_cube_human_image",
         "lerobot/aloha_sim_transfer_cube_scripted_image",
     ],
-    "pusht": ["lerobot/pusht", "lerobot/pusht_image", "lerobot/pusht_keypoints"],
+    # TODO(alexander-soare): Add "lerobot/pusht_keypoints". Right now we can't because this is too tightly
+    # coupled with tests.
+    "pusht": ["lerobot/pusht", "lerobot/pusht_image"],
     "xarm": [
         "lerobot/xarm_lift_medium",
         "lerobot/xarm_lift_medium_replay",
