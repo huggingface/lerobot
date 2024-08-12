c6086bbf47a03af96dbb75065902252af0049991
Remi Cadene <re.cadene@gmail.com>
2024-08-12 16:04:00 +0200

    fix core dump

--- lerobot/scripts/control_robot.py
+++ lerobot/scripts/control_robot.py
@@ -834,3 +834,7 @@ if __name__ == "__main__":
 
     elif control_mode == "replay":
         replay(robot, **kwargs)
+
+    # Disconnect manually to avoid a "Core dump" during process
+    # termination due to camera threads not properly exiting.
+    robot.disconnect()
