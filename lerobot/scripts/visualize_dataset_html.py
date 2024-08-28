72969568aefd2a2d7261af9fe76445a6623e4e64
Michel Aractingi <michel.aractingi@huggingface.co>
2024-08-28 09:44:06 +0200

    nit

--- lerobot/scripts/visualize_dataset_html.py
+++ lerobot/scripts/visualize_dataset_html.py
@@ -199,10 +199,11 @@ def get_episode_language_instruction(dataset: LeRobotDataset, ep_index: int) -> 
     first_frame_idx = dataset.episode_data_index["from"][ep_index].item()
 
     language_instruction = dataset.hf_dataset[first_frame_idx]["language_instruction"]
-    # hack to remove the prefix and suffix in open x that were badly stored
-    language_instruction = language_instruction.removeprefix("tf.Tensor(b'").removesuffix(
-        "', shape=(), dtype=string)"
-    )
+    # TODO (michel-aractingi) hack to get the sentence, some strings in openx are badly stored
+    # with the tf.tensor appearing in the string
+    language_instruction = language_instruction \
+                .removeprefix("tf.Tensor(b'") \
+                .removesuffix("', shape=(), dtype=string)")
     return language_instruction
 
 
