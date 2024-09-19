fdfee65fd56bdb00c37d2f9ff8482ce67fec3010
Simon Alibert <simon.alibert@huggingface.co>
2024-09-19 10:32:07 +0200

    Improve mock_opencv

--- tests/mock_opencv.py
+++ tests/mock_opencv.py
@@ -14,15 +14,20 @@ class MockVideoCapture(cv2.VideoCapture):
             cv2.CAP_PROP_FRAME_WIDTH: 640,
             cv2.CAP_PROP_FRAME_HEIGHT: 480,
         }
+        self._is_opened = True
 
     def isOpened(self):  # noqa: N802
         return True
 
     def set(self, propId: int, value: float) -> bool:  # noqa: N803
+        if not self._is_opened:
+            raise RuntimeError("Camera is not opened")
         self._mock_dict[propId] = value
         return True
 
     def get(self, propId: int) -> float:  # noqa: N803
+        if not self._is_opened:
+            raise RuntimeError("Camera is not opened")
         value = self._mock_dict[propId]
         if value == 0:
             if propId == cv2.CAP_PROP_FRAME_HEIGHT:
@@ -32,10 +37,16 @@ class MockVideoCapture(cv2.VideoCapture):
         return value
 
     def read(self):
+        if not self._is_opened:
+            raise RuntimeError("Camera is not opened")
         h = self.get(cv2.CAP_PROP_FRAME_HEIGHT)
         w = self.get(cv2.CAP_PROP_FRAME_WIDTH)
         ret = True
         return ret, self.image[f"{h}x{w}"]
 
     def release(self):
-        pass
+        self._is_opened = False
+
+    def __del__(self):
+        if self._is_opened:
+            self.release()
