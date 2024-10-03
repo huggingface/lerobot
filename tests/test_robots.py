f9237898d25349773a2fe3843eaff0245f7dbc1f
Remi Cadene <re.cadene@gmail.com>
2024-10-03 16:30:07 +0200

    fix with **robot_kwargs

--- tests/test_robots.py
+++ tests/test_robots.py
@@ -40,6 +40,8 @@ def test_robot(tmpdir, request, robot_type, mock):
     # TODO(rcadene): test logs
     # TODO(rcadene): add compatibility with other robots
 
+    robot_kwargs = {"robot_type": robot_type}
+
     if robot_type == "aloha" and mock:
         # To simplify unit test, we do not rerun manual calibration for Aloha mock=True.
         # Instead, we use the files from '.cache/calibration/aloha_default'
@@ -52,15 +54,16 @@ def test_robot(tmpdir, request, robot_type, mock):
         tmpdir = Path(tmpdir)
         calibration_dir = tmpdir / robot_type
         overrides_calibration_dir = [f"calibration_dir={calibration_dir}"]
+        robot_kwargs["calibration_dir"] = calibration_dir
 
     # Test connecting without devices raises an error
-    robot = ManipulatorRobot(calibration_dir=calibration_dir)
+    robot = ManipulatorRobot(**robot_kwargs)
     with pytest.raises(ValueError):
         robot.connect()
     del robot
 
     # Test using robot before connecting raises an error
-    robot = ManipulatorRobot(calibration_dir=calibration_dir)
+    robot = ManipulatorRobot(**robot_kwargs)
     with pytest.raises(RobotDeviceNotConnectedError):
         robot.teleop_step()
     with pytest.raises(RobotDeviceNotConnectedError):
