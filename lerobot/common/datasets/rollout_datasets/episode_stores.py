8bd616ddf695dab773ff6d6ceda02b232d3906b4
sm <profile2697@gmail.com>
2024-11-07 17:05:34 +0900

    feat: episode store

--- lerobot/common/datasets/rollout_datasets/episode_stores.py
+++ lerobot/common/datasets/rollout_datasets/episode_stores.py
@@ -1,12 +1,13 @@
+from pathlib import Path
+
+import numcodecs
+import numpy as np
 import torch
 import zarr
-import numpy as np
-import numcodecs
+
 from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
-
+from lerobot.common.datasets.rollout_datasets.zarr_utils import to_hf_dataset, read_data_from_zarr
 from lerobot.common.datasets.utils import calculate_episode_data_index
-
-from lerobot.common.datasets.push_dataset_to_hub.pusht_zarr_format import to_hf_dataset
 
 
 def _append_episode_to_zarr(ep_dict, zarr_group):
@@ -27,45 +28,47 @@ def _append_episode_to_zarr(ep_dict, zarr_group):
         zarr_group[key].append(value)
 
 
-def _read_dataset_from_zarr(zarr_group):
-    data_dict = {}
-    for k, v in zarr_group.items():
-        data_dict[k] = v if not isinstance(v, np.ndarray) else torch.from_numpy(v)
-    return data_dict
-
-
 class EpisodeVideoStore(object):
-    def __init__(self, root_group: zarr.Group, info: dict = None, num_episodes: int = None):
+    def __init__(self, root_group: zarr.Group, info: dict = None):
         super().__init__()
         self.root_group = root_group
-        self.root_group.attrs.put('info', info)
-        self._num_episodes = num_episodes if num_episodes else self._get_num_episodes()
+        if 'info' in self.root_group.attrs:
+            print("Connected to the exising zarr")
+        else:
+            print("Create a new zarr")
+
+        if info is not None:
+            self.root_group.attrs['info'] = info
+        if 'num_episodes' not in self.root_group.attrs:
+            self.root_group.attrs['num_episodes'] = 0
 
     def add_episode(self, episode_dict):
-        assert self._num_episodes <= episode_dict['episode_index'][0]
+        assert 'episode_index' not in episode_dict
+        assert 'frame_index' in episode_dict
 
+        episode_dict['episode_index'] = torch.tensor([self.num_episodes] * len(episode_dict['frame_index']),
+                                                     dtype=torch.int64)
         episode_dict = self._cleansing_episode_dict(episode_dict)
         _append_episode_to_zarr(episode_dict, self.root_group)
-        self._num_episodes += 1
+        self.root_group.attrs['num_episodes'] += 1
 
-    def convert_to_lerobot_dataset(self):
-        # TODO: large data
-        data_dict = _read_dataset_from_zarr(self.root_group)
-        hf_dataset = to_hf_dataset(data_dict, video=True, keypoints_instead_of_image=False)
+    def convert_to_lerobot_dataset(self, repo_id):
+        zarr_dict = read_data_from_zarr(self.root_group)
+        hf_dataset = to_hf_dataset(zarr_dict)
         episode_data_index = calculate_episode_data_index(hf_dataset)
 
-        info = self.root_group.attrs.asdict()
+        info = self.info
         return LeRobotDataset.from_preloaded(
-            repo_id='place/holder',
+            repo_id=repo_id,
             hf_dataset=hf_dataset,
             episode_data_index=episode_data_index,
             info=info,
-            videos_dir=info['videos_dir'],
+            videos_dir=Path(info['videos_dir']),
         )
 
     @classmethod
     def create_from_path(cls, root_path, info: dict = None, mode='r'):
-        assert 'video_dir' in info
+        assert mode == 'r' or 'videos_dir' in info
         root_group = zarr.open(root_path, mode=mode)
         return EpisodeVideoStore(root_group, info)
 
@@ -73,19 +76,9 @@ class EpisodeVideoStore(object):
     def info(self):
         return self.root_group.attrs.get('info')
 
+    @property
+    def num_episodes(self):
+        return self.root_group.attrs['num_episodes']
+
     def _cleansing_episode_dict(self, episode_dict):
         return {k: v.cpu().numpy() if isinstance(v, torch.Tensor) else v for k, v in episode_dict.items()}
-
-    def _get_num_episodes(self):
-        episode_index_dataset = self.root_group['episode_index']
-        total_size = episode_index_dataset.shape[0]
-        chunk_size = episode_index_dataset.chunks[0]
-
-        max_episode_index = -1
-        for idx in range(0, total_size, chunk_size):
-            end_idx = max(idx + chunk_size, total_size - 1)
-            episode_index_chunk = episode_index_dataset[idx:end_idx]
-            chunk_max_episode_index = np.max(episode_index_chunk)
-            if chunk_max_episode_index > max_episode_index:
-                max_episode_index = chunk_max_episode_index
-        return max_episode_index + 1
---
+++ lerobot/common/datasets/rollout_datasets/zarr_utils.py
@@ -0,0 +1,54 @@
+from typing import Dict
+
+import numpy as np
+import torch
+import zarr
+from datasets import Sequence, Value, Dataset, Features
+
+from lerobot.common.datasets.utils import hf_transform_to_torch
+from lerobot.common.datasets.video_utils import VideoFrame
+
+
+def zarr_data_generator(zarr_dict: Dict[str, zarr.core.Array]):
+    data_size = _check_sanity_and_get_data_size(zarr_dict)
+    for i in range(data_size):
+        yield {k: v[i] if not isinstance(v[i], np.ndarray) else torch.from_numpy(v[i]) for k, v in zarr_dict.items()}
+
+
+def _check_sanity_and_get_data_size(zarr_dict: Dict[str, zarr.core.Array]):
+    array_lengths = [v.shape[0] for v in zarr_dict.values()]
+    for p, n in zip(array_lengths, array_lengths[1:]):
+        assert p == n
+    return array_lengths[0]
+
+
+def to_hf_dataset(zarr_dict: Dict[str, zarr.core.Array]):
+    features = {}
+
+    features["observation.image"] = VideoFrame()
+    features["observation.state"] = Sequence(
+        length=zarr_dict["observation.state"].shape[1], feature=Value(dtype="float32", id=None)
+    )
+    features["action"] = Sequence(
+        length=zarr_dict["action"].shape[1], feature=Value(dtype="float32", id=None)
+    )
+    features["episode_index"] = Value(dtype="int64", id=None)
+    features["frame_index"] = Value(dtype="int64", id=None)
+    features["timestamp"] = Value(dtype="float32", id=None)
+    features["next.reward"] = Value(dtype="float32", id=None)
+    features["next.done"] = Value(dtype="bool", id=None)
+    features["next.success"] = Value(dtype="bool", id=None)
+    features["index"] = Value(dtype="int64", id=None)
+
+    hf_dataset = Dataset.from_generator(zarr_data_generator,
+                                        gen_kwargs={'zarr_dict': zarr_dict},
+                                        features=Features(features))
+    hf_dataset.set_transform(hf_transform_to_torch)
+    return hf_dataset
+
+
+def read_data_from_zarr(zarr_group) -> Dict[str, zarr.core.Array]:
+    data_dict = {}
+    for k, v in zarr_group.items():
+        data_dict[k] = v if not isinstance(v, np.ndarray) else torch.from_numpy(v)
+    return data_dict
