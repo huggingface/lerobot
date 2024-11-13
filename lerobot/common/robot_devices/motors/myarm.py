697eef5afadc69cd3ce5324f630ac7e0786d1b9a
Alex Thiele <alex@urbanmachine.build>
2024-11-13 10:15:49 -0800

    Add yet-to-be-implemented myarm wrappers

---
+++ lerobot/common/robot_devices/motors/myarm.py
@@ -0,0 +1,66 @@
+from abc import abstractmethod, ABC
+from enum import Enum
+from typing import Literal
+
+import acton_ai
+import numpy as np
+import numpy.typing as npt
+
+
+class TorqueMode(Enum):
+    ENABLED = 1
+    DISABLED = 0
+
+
+class MyArmBaseClass(ABC):
+
+    def __init__(
+            self,
+            port: str,
+            motors: dict[str, tuple[int, str]],
+            extra_model_control_table: dict[str, list[tuple]] | None = None,
+            extra_model_resolution: dict[str, int] | None = None,
+            mock=False,
+    ):
+        self.port = port
+        self.motors = motors
+        self.mock = mock
+
+    @abstractmethod
+    def connect(self) -> None:
+        pass
+
+
+class MyArmLeader(MyArmBaseClass):
+    _handle: acton_ai.MyArmC
+    """Instantiated in `connect`"""
+
+    def connect(self) -> None:
+        self._handle = acton_ai.find_myarm_controller()
+
+    def read(self, cmd: Literal["Present_Position"]) -> npt.NDArray[np.float64]:
+        match cmd:
+            case "Present_Position":
+                raise NotImplementedError()
+            case _:
+                raise ValueError(f"Unsupported {cmd=}")
+
+
+class MyArmFollower(MyArmBaseClass):
+    _handle: acton_ai.HelpfulMyArmM
+    """Instantiated in `connect`"""
+
+    def connect(self) -> None:
+        self._handle = acton_ai.find_myarm_motor()
+
+    def write(self, cmd: Literal["Goal_Position"],
+              data: npt.NDArray[np.float64]) -> None:
+        """This should mirror 'read' in the order of the data"""
+        # TODO: Implement
+        match cmd:
+            case "Goal_Position":
+                raise NotImplementedError()
+            case "Torque_Enable":
+                self._handle.bring_up_motors()
+            case _:
+                raise ValueError(f"Unsupported {cmd=}")
