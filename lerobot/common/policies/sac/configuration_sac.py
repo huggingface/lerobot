5ae5dcb30f5c1e81cf388155138ef8afb1df8ac4
KeWang <superwk1017@gmail.com>
2024-12-17 13:26:17 +0000

    Port SAC WIP (#581)
    Co-authored-by: KeWang1017 <ke.wang@helloleap.ai>

--- lerobot/common/policies/sac/configuration_sac.py
+++ lerobot/common/policies/sac/configuration_sac.py
@@ -21,3 +21,19 @@ from dataclasses import dataclass
 @dataclass
 class SACConfig:
     discount = 0.99
+    temperature_init = 1.0
+    num_critics = 2
+    critic_lr = 3e-4
+    actor_lr = 3e-4
+    critic_network_kwargs = {
+            "hidden_dims": [256, 256],
+            "activate_final": True,
+        }
+    actor_network_kwargs = {
+            "hidden_dims": [256, 256],
+            "activate_final": True,
+        }
+    policy_kwargs = {
+            "tanh_squash_distribution": True,
+            "std_parameterization": "uniform",
+        }
