3551dc501596d95846529c4140b5ac1155f9a344
Simon Alibert <simon.alibert@huggingface.co>
2025-01-28 09:55:46 +0100

    Update factories docstrings

--- lerobot/common/optim/factory.py
+++ lerobot/common/optim/factory.py
@@ -29,6 +29,15 @@ from lerobot.configs.train import TrainPipelineConfig
 def make_optimizer_and_scheduler(
     cfg: TrainPipelineConfig, policy: PreTrainedPolicy
 ) -> tuple[Optimizer, LRScheduler | None]:
+    """Generates the optimizer and scheduler based on configs.
+
+    Args:
+        cfg (TrainPipelineConfig): The training config that contains optimizer and scheduler configs
+        policy (PreTrainedPolicy): The policy config from which parameters and presets must be taken from.
+
+    Returns:
+        tuple[Optimizer, LRScheduler | None]: The couple (Optimizer, Scheduler). Scheduler can be `None`.
+    """
     params = policy.get_optim_params() if cfg.use_policy_training_preset else policy.parameters()
     optimizer = cfg.optimizer.build(params)
     lr_scheduler = cfg.scheduler.build(optimizer, cfg.offline.steps) if cfg.scheduler is not None else None
