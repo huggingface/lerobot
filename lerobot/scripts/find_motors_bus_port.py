114a381204284aefb18d21abdc05b76e0723f9c2
ma3oun <berjaoui@gmail.com>
2025-02-09 16:29:48 +0100

    Added GUI support

--- lerobot/scripts/find_motors_bus_port.py
+++ lerobot/scripts/find_motors_bus_port.py
@@ -4,6 +4,8 @@ from pathlib import Path
 
 from serial.tools import list_ports  # Part of pyserial library
 
+from lerobot.common.utils.gui import PromptGUI
+
 
 def find_available_ports():
     if os.name == "nt":  # Windows
@@ -15,26 +17,54 @@ def find_available_ports():
     return ports
 
 
-def find_port():
-    print("Finding all available ports for the MotorsBus.")
+def find_port_core(display_message, wait_for_input):
+    display_message("Finding all available ports for the MotorsBus.")
     ports_before = find_available_ports()
-    print("Ports before disconnecting:", ports_before)
-
-    print("Remove the USB cable from your MotorsBus and press Enter when done.")
-    input()  # Wait for user to disconnect the device
-
+    display_message(f"Ports before disconnecting: {ports_before}", 14)
+    display_message("Remove the USB cable from your MotorsBus and press 'OK' when done.")
+    wait_for_input()
     time.sleep(0.5)  # Allow some time for port to be released
     ports_after = find_available_ports()
     ports_diff = list(set(ports_before) - set(ports_after))
-
     if len(ports_diff) == 1:
         port = ports_diff[0]
-        print(f"The port of this MotorsBus is '{port}'")
-        print("Reconnect the USB cable.")
+        display_message(f"The port of this MotorsBus is '{port}'")
+        display_message("Reconnect the USB cable.", button="Exit")
+        wait_for_input()
+        return port
     elif len(ports_diff) == 0:
         raise OSError(f"Could not detect the port. No difference was found ({ports_diff}).")
     else:
         raise OSError(f"Could not detect the port. More than one port was found ({ports_diff}).")
+
+
+def find_port_with_gui():
+    gui = PromptGUI("Motors Bus Port Detector")
+
+    def display_message(message, size=24, button="OK"):
+        gui.add_step(message, size, button)
+
+    def wait_for_input():
+        gui.run()
+
+    try:
+        find_port_core(display_message, wait_for_input)
+    except OSError as e:
+        display_message(e)
+        wait_for_input()
+    finally:
+        gui.terminate()
+
+
+def find_port_with_console():
+    return find_port_core(print, input)
+
+
+def find_port():
+    if os.getenv("LEROBOT_GUI", "False").lower() in ["true", "1", "t"]:
+        return find_port_with_gui()
+    else:
+        return find_port_with_console()
 
 
 if __name__ == "__main__":
