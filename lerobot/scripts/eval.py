4d9d34ed161b4a0df9818147b526aa164f274575
Ilia Larchenko <41329713+IliaLarchenko@users.noreply.github.com>
2025-02-10 21:01:19 +0700

    format fix

--- lerobot/scripts/eval.py
+++ lerobot/scripts/eval.py
@@ -155,8 +155,7 @@ def rollout(
             all_observations.append(deepcopy(observation))
 
         observation = {
-            key: observation[key].to(device, non_blocking=device.type == "cuda")
-            for key in observation
+            key: observation[key].to(device, non_blocking=device.type == "cuda") for key in observation
         }
 
         with torch.inference_mode():
