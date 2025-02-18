91a344fadbd6c928a9ae6b6f9fd1ec0327a32dea
Pepijn <pepijn@huggingface.co>
2025-02-18 09:40:52 +0100

    not needed

--- lerobot/common/robot_devices/control_utils.py
+++ lerobot/common/robot_devices/control_utils.py
@@ -44,7 +44,7 @@ def log_control_info(robot: Robot, dt_s, episode_index=None, frame_index=None, f
     log_dt("dt", dt_s)
 
     # TODO(aliberts): move robot-specific logs logic in robot.print_logs()
-    if not (robot.robot_type.startswith("stretch") or robot.robot_type.startswith("mobile_so100")):
+    if not robot.robot_type.startswith("stretch"):
         for name in robot.leader_arms:
             key = f"read_leader_{name}_pos_dt_s"
             if key in robot.logs:
