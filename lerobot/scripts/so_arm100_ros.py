980c3db7803fc9a9de7cb74df3efb83e555a1ec3
ZhuYaoHui1998 <510677932@qq.com>
2025-02-18 17:35:38 +0800

    create dev branch

--- lerobot/scripts/so_arm100_ros.py
+++ lerobot/scripts/so_arm100_ros.py
@@ -70,59 +70,59 @@
 ############################################################################################
 ######手动控制机械臂N次记录当前位置，并依次执行 Demo4
 
-# from lerobot.common.robot_devices.motors.feetech import *
-# from lerobot.common.robot_devices.robots.configs import So100RobotConfig
-# from lerobot.common.robot_devices.robots.manipulator import ManipulatorRobot
-# import numpy as np
-# import time
+from lerobot.common.robot_devices.motors.feetech import *
+from lerobot.common.robot_devices.robots.configs import So100RobotConfig
+from lerobot.common.robot_devices.robots.manipulator import ManipulatorRobot
+import numpy as np
+import time
 
-# # 初始化机器人配置
-# so_arm100_configs = So100RobotConfig()
+# 初始化机器人配置
+so_arm100_configs = So100RobotConfig()
 
-# config = FeetechMotorsBusConfig(
-#     port=so_arm100_configs.follower_arms["main"].port,
-#     motors=so_arm100_configs.follower_arms["main"].motors
-# )
+config = FeetechMotorsBusConfig(
+    port=so_arm100_configs.follower_arms["main"].port,
+    motors=so_arm100_configs.follower_arms["main"].motors
+)
 
-# # 创建机器人对象
-# robot = ManipulatorRobot(so_arm100_configs)
+# 创建机器人对象
+robot = ManipulatorRobot(so_arm100_configs)
 
-# # 连接到机器人
-# robot.connect()
+# 连接到机器人
+robot.connect()
 
-# # 记录角度的列表
-# positions = []
+# 记录角度的列表
+positions = []
 
-# # 示教次数
-# pose_index= 3
+# 示教次数
+pose_index= 3
 
-# # 取消自锁
-# robot.follower_arms["main"].write("Torque_Enable", TorqueMode.DISABLED.value) 
+# 取消自锁
+robot.follower_arms["main"].write("Torque_Enable", TorqueMode.DISABLED.value) 
 
-# # 手动操作三次
-# for i in range(pose_index):
-#     input(f"第 {i+1} 次手动操作，请按回车记录当前位置...")
+# 手动操作三次
+for i in range(pose_index):
+    input(f"第 {i+1} 次手动操作，请按回车记录当前位置...")
     
-#     # 读取当前位置
-#     follower_pos = robot.follower_arms["main"].read("Present_Position")
+    # 读取当前位置
+    follower_pos = robot.follower_arms["main"].read("Present_Position")
     
-#     # 打印当前位置
-#     print(f"当前机械臂角度：{np.array2string(follower_pos, precision=3, suppress_small=True)}")
+    # 打印当前位置
+    print(f"当前机械臂角度：{np.array2string(follower_pos, precision=3, suppress_small=True)}")
     
-#     # 将当前位置添加到列表中
-#     positions.append(follower_pos)
+    # 将当前位置添加到列表中
+    positions.append(follower_pos)
     
-# # 开启自锁
-# robot.follower_arms["main"].write("Torque_Enable", TorqueMode.ENABLED.value) 
+# 开启自锁
+robot.follower_arms["main"].write("Torque_Enable", TorqueMode.ENABLED.value) 
 
-# # 显示记录的所有位置，并执行
-# print("\n所有记录的角度：")
-# for i, pos in enumerate(positions, 1):
-#     time.sleep(3)
-#     print(f"第 {i} 次记录的位置：{np.array2string(pos, precision=3, suppress_small=True)}")
-#     print(f"\n机械臂将自动移动到目标位置：{np.array2string(pos, precision=3, suppress_small=True)}")
-#     # 让机械臂移动到目标位置
-#     robot.follower_arms["main"].write("Goal_Position", pos)
+# 显示记录的所有位置，并执行
+print("\n所有记录的角度：")
+for i, pos in enumerate(positions, 1):
+    time.sleep(1)
+    print(f"第 {i} 次记录的位置：{np.array2string(pos, precision=3, suppress_small=True)}")
+    print(f"机械臂将自动移动到目标位置：{np.array2string(pos, precision=3, suppress_small=True)}")
+    # 让机械臂移动到目标位置
+    robot.follower_arms["main"].write("Goal_Position", pos)
     
-# # 断开连接
-# robot.disconnect()
+# 断开连接
+robot.disconnect()
