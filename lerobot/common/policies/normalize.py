0d88a5ee091442b51afe5c0b7931138b8494ae7a
Michel Aractingi <michel.aractingi@huggingface.co>
2025-02-19 16:22:51 +0000

    - Fixed big issue in the loading of the policy parameters sent by the learner to the actor -- pass only the actor to the `update_policy_parameters` and remove `strict=False` - Fixed big issue in the normalization of the actions in the `forward` function of the critic -- remove the `torch.no_grad` decorator in `normalize.py` in the normalization function - Fixed performance issue to boost the optimization frequency by setting the storage device to be the same as the device of learning.
    Co-authored-by: Adil Zouitine <adilzouitinegm@gmail.com>

--- lerobot/common/policies/normalize.py
+++ lerobot/common/policies/normalize.py
@@ -149,7 +149,7 @@ class Normalize(nn.Module):
             setattr(self, "buffer_" + key.replace(".", "_"), buffer)
 
     # TODO(rcadene): should we remove torch.no_grad?
-    @torch.no_grad
+    # @torch.no_grad
     def forward(self, batch: dict[str, Tensor]) -> dict[str, Tensor]:
         batch = dict(batch)  # shallow copy avoids mutating the input batch
         for key, ft in self.features.items():
