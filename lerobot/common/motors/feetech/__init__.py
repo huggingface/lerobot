5ab418dbeb038476cf761b0b8a4ccadf25cb2b6b
Simon Alibert <simon.alibert@huggingface.co>
2025-03-03 20:17:54 +0100

    Add feetech calibration

--- lerobot/common/motors/feetech/__init__.py
+++ lerobot/common/motors/feetech/__init__.py
@@ -1,3 +1,4 @@
 from .feetech import FeetechMotorsBus, TorqueMode
+from .feetech_calibration import run_arm_manual_calibration
 
-__all__ = ["FeetechMotorsBus", "TorqueMode"]
+__all__ = ["FeetechMotorsBus", "TorqueMode", "run_arm_manual_calibration"]
