e1d6a6f00efbe1b3aedb0fc3ca923b604a57e31d
Steven Palma <steven.palma@huggingface.co>
2025-03-17 18:49:05 +0100

    fix(lekiwi): HW fixes v0.4

--- lerobot/common/robots/lekiwi/configuration_lekiwi.py
+++ lerobot/common/robots/lekiwi/configuration_lekiwi.py
@@ -16,10 +16,10 @@ class LeKiwiRobotConfig(RobotConfig):
     cameras: dict[str, CameraConfig] = field(
         default_factory=lambda: {
             "front": OpenCVCameraConfig(
-                camera_index="/dev/video0", fps=30, width=640, height=480, rotation=90
+                camera_index="/dev/video1", fps=30, width=640, height=480, rotation=90
             ),
             "wrist": OpenCVCameraConfig(
-                camera_index="/dev/video2", fps=30, width=640, height=480, rotation=180
+                camera_index="/dev/video4", fps=30, width=640, height=480, rotation=180
             ),
         }
     )
