6a77189f50cd236ca88214ed8931264e1770f8f6
Simon Alibert <simon.alibert@huggingface.co>
2025-03-19 19:02:58 +0100

    Fix import

--- lerobot/common/motors/dynamixel/__init__.py
+++ lerobot/common/motors/dynamixel/__init__.py
@@ -1,4 +1,4 @@
-from .dynamixel import DynamixelMotorsBus, set_operating_mode
+from .dynamixel import DynamixelMotorsBus
 from .dynamixel_calibration import run_arm_calibration
 
-__all__ = ["DynamixelMotorsBus", "set_operating_mode", "run_arm_calibration"]
+__all__ = ["DynamixelMotorsBus", "run_arm_calibration"]
