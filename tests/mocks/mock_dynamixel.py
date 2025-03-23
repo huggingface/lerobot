ff7cfdaf400fcb4058c85ffc2d3acc1d19e03af7
Simon Alibert <simon.alibert@huggingface.co>
2025-03-23 17:03:04 +0100

    Move mock_serial patch to dedicated file

--- tests/mocks/mock_dynamixel.py
+++ tests/mocks/mock_dynamixel.py
@@ -1,14 +1,14 @@
 import abc
 import random
-import threading
-import time
 from typing import Callable
 
 import dynamixel_sdk as dxl
 import serial
-from mock_serial.mock_serial import MockSerial, Stub
+from mock_serial.mock_serial import MockSerial
 
 from lerobot.common.motors.dynamixel import X_SERIES_CONTROL_TABLE, DynamixelMotorsBus
+
+from .mock_serial_patch import WaitableStub
 
 # https://emanual.robotis.com/docs/en/dxl/crc/
 DXL_CRC_TABLE = [
@@ -418,34 +418,6 @@ class MockPortHandler(dxl.PortHandler):
         self.tx_time_per_byte = (1000.0 / self.baudrate) * 10.0
 
         return True
-
-
-class WaitableStub(Stub):
-    """
-    In some situations, a test might be checking if a stub has been called before `MockSerial` thread had time
-    to read, match, and call the stub. In these situations, the test can fail randomly.
-
-    Use `wait_called()` or `wait_calls()` to block until the stub is called, avoiding race conditions.
-    """
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self._event = threading.Event()
-
-    def call(self):
-        self._event.set()
-        return super().call()
-
-    def wait_called(self, timeout: float = 1.0):
-        return self._event.wait(timeout)
-
-    def wait_calls(self, min_calls: int = 1, timeout: float = 1.0):
-        start = time.perf_counter()
-        while time.perf_counter() - start < timeout:
-            if self.calls >= min_calls:
-                return self.calls
-            time.sleep(0.005)
-        raise TimeoutError(f"Stub not called {min_calls} times within {timeout} seconds.")
 
 
 class MockMotors(MockSerial):
