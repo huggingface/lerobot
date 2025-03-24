8755a63a0a922d6dfb634c1bc54d6ccd0b4814ba
Gabriel Cordeiro Bosse <gabrielbosse25gmail.com>
2025-03-24 16:30:41 -0300

    Adicionei a policy act-language

--- lerobot/common/policies/factory.py
+++ lerobot/common/policies/factory.py
@@ -23,6 +23,7 @@ from lerobot.common.datasets.utils import dataset_to_policy_features
 from lerobot.common.envs.configs import EnvConfig
 from lerobot.common.envs.utils import env_to_policy_features
 from lerobot.common.policies.act.configuration_act import ACTConfig
+from lerobot.common.policies.actlanguage.configuration_act import ACTLanguageConfig
 from lerobot.common.policies.diffusion.configuration_diffusion import DiffusionConfig
 from lerobot.common.policies.pi0.configuration_pi0 import PI0Config
 from lerobot.common.policies.pretrained import PreTrainedPolicy
@@ -46,6 +47,10 @@ def get_policy_class(name: str) -> PreTrainedPolicy:
         from lerobot.common.policies.act.modeling_act import ACTPolicy
 
         return ACTPolicy
+    elif name == "actlanguage":
+        from lerobot.common.policies.actlanguage.modeling_act import ACTLanguagePolicy
+
+        return ACTLanguagePolicy
     elif name == "vqbet":
         from lerobot.common.policies.vqbet.modeling_vqbet import VQBeTPolicy
 
@@ -65,6 +70,8 @@ def make_policy_config(policy_type: str, **kwargs) -> PreTrainedConfig:
         return DiffusionConfig(**kwargs)
     elif policy_type == "act":
         return ACTConfig(**kwargs)
+    elif policy_type == "actlanguage":
+        return ACTLanguageConfig(**kwargs)
     elif policy_type == "vqbet":
         return VQBeTConfig(**kwargs)
     elif policy_type == "pi0":
