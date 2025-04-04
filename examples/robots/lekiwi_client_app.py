b919e68e055d9884c7a786ae1408bbd9f3c89646
Steven Palma <steven.palma@huggingface.co>
2025-04-04 17:53:29 +0200

    debug(examples): configuration for lekiwi client app

--- examples/robots/lekiwi_client_app.py
+++ examples/robots/lekiwi_client_app.py
@@ -14,7 +14,6 @@
 
 import logging
 
-from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
 from lerobot.common.robots.config import RobotMode
 from lerobot.common.robots.lekiwi.config_lekiwi import LeKiwiClientConfig
 from lerobot.common.robots.lekiwi.lekiwi_client import LeKiwiClient
@@ -79,7 +78,7 @@ DUMMY_FEATURES = {
 
 def main():
     logging.info("Configuring Teleop Devices")
-    leader_arm_config = SO100LeaderConfig(port="/dev/tty.usbmodem58760429271")
+    leader_arm_config = SO100LeaderConfig(port="/dev/tty.usbmodem58760434171")
     leader_arm = SO100Leader(leader_arm_config)
 
     keyboard_config = KeyboardTeleopConfig()
@@ -93,12 +92,12 @@ def main():
 
     logging.info("Creating LeRobot Dataset")
 
-    # TODO(Steven): Check this creation
-    dataset = LeRobotDataset.create(
-        repo_id="user/lekiwi",
-        fps=10,
-        features=DUMMY_FEATURES,
-    )
+    # # TODO(Steven): Check this creation
+    # dataset = LeRobotDataset.create(
+    #     repo_id="user/lekiwi2",
+    #     fps=10,
+    #     features=DUMMY_FEATURES,
+    # )
 
     logging.info("Connecting Teleop Devices")
     leader_arm.connect()
@@ -107,12 +106,16 @@ def main():
     logging.info("Connecting remote LeKiwi")
     robot.connect()
 
+    if not robot.is_connected or not leader_arm.is_connected or not keyboard.is_connected:
+        logging.error("Failed to connect to all devices")
+        return
+
     logging.info("Starting LeKiwi teleoperation")
     i = 0
     while i < 1000:
         arm_action = leader_arm.get_action()
         base_action = keyboard.get_action()
-        action = {**arm_action, **base_action} if base_action.size > 0 else arm_action
+        action = {**arm_action, **base_action} if len(base_action) > 0 else arm_action
 
         # TODO(Steven): Deal with policy action space
         # robot.set_mode(RobotMode.AUTO)
@@ -126,10 +129,10 @@ def main():
         frame.update({"task": "Dummy Task Dataset"})
 
         logging.info("Saved a frame into the dataset")
-        dataset.add_frame(frame)
+        # dataset.add_frame(frame)
         i += 1
 
-    dataset.save_episode()
+    # dataset.save_episode()
     # dataset.push_to_hub()
 
     logging.info("Disconnecting Teleop Devices and LeKiwi Client")
