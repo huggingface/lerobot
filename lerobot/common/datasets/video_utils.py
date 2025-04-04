faf0c57511cde7dd8333326b4b736690d6d29a81
CarolinePascal <caroline8.pascal@gmail.com>
2025-04-04 18:31:00 +0200

    Adding pytorch compatible conversion for audio

--- lerobot/common/datasets/video_utils.py
+++ lerobot/common/datasets/video_utils.py
@@ -85,6 +85,7 @@ def decode_audio_torchvision(
     reader.add_basic_audio_stream(
         frames_per_chunk = int(ceil(duration * audio_sampling_rate)),    #Too much is better than not enough
         buffer_chunk_size = -1, #No dropping frames
+        format = "fltp",    #Format as float32
     )
 
     audio_chunks = []
@@ -102,7 +103,6 @@ def decode_audio_torchvision(
         audio_chunks.append(current_audio_chunk)
 
     audio_chunks = torch.stack(audio_chunks)
-    #TODO(CarolinePascal) : pytorch format conversion ?
 
     assert len(timestamps) == len(audio_chunks)
     return audio_chunks
