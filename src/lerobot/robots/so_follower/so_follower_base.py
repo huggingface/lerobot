bec92c1c560d80a0a43fc80b9a89ba565efc1bfe
CarolinePascal <caroline8.pascal@gmail.com>
2025-04-09 14:59:29 +0200

    Adding multiprocessing support for audio recording

--- src/lerobot/robots/so_follower/so_follower_base.py
+++ src/lerobot/robots/so_follower/so_follower_base.py
@@ -76,7 +76,8 @@ class SOFollowerBase(Robot):
     @property
     def _microphones_ft(self) -> dict[str, tuple]:
         return {
-            mic: (self.config.microphones[mic].sample_rate, self.config.microphones[mic].channels) for mic in self.microphones
+            mic: (self.config.microphones[mic].sample_rate, self.config.microphones[mic].channels)
+            for mic in self.microphones
         }
 
     @cached_property
