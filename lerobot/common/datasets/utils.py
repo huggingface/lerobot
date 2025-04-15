f70d54434602614fba10868d4e38f9a6b5f886b8
坂本滉也 <k-sakamoto@KoyaBookPro.local>
2025-04-15 20:50:48 +0900

    debug: change the data type of expected_shape to tuple

--- lerobot/common/datasets/utils.py
+++ lerobot/common/datasets/utils.py
@@ -757,7 +757,7 @@ def validate_feature_numpy_array(
         if actual_dtype != np.dtype(expected_dtype):
             error_message += f"The feature '{name}' of dtype '{actual_dtype}' is not of the expected dtype '{expected_dtype}'.\n"
 
-        if actual_shape != expected_shape:
+        if actual_shape != tuple(expected_shape):
             error_message += f"The feature '{name}' of shape '{actual_shape}' does not have the expected shape '{expected_shape}'.\n"
     else:
         error_message += f"The feature '{name}' is not a 'np.ndarray'. Expected type is '{expected_dtype}', but type '{type(value)}' provided instead.\n"
