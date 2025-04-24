4ace6f4d4a50c03f038948aa68d6334d0f162c8a
Eugene Mironov <helper2424@gmail.com>
2025-04-24 15:25:54 +0700

    Add Remote policy and remote config

---
+++ lerobot/common/policies/remote/modeling_remote.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+# Copyright 2024 Tony Z. Zhao and The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Action Chunking Transformer Policy
+
+As per Learning Fine-Grained Bimanual Manipulation with Low-Cost Hardware (https://arxiv.org/abs/2304.13705).
+The majority of changes here involve removing unused code, unifying naming, and adding helpful comments.
+"""
+
+from collections import deque
+
+import torch
+from torch import Tensor
+
+from lerobot.common.policies.pretrained import PreTrainedPolicy
+
+
+class RemotePolicy(PreTrainedPolicy):
+    config_class = RemoteConfig
+    name = "remote"
+
+    def __init__(
+        self,
+        config: RemoteConfig,
+        dataset_stats: dict[str, dict[str, Tensor]] | None = None,
+    ):
+        """
+        Args:
+            config: Policy configuration class instance or None, in which case the default instantiation of
+                    the configuration class is used.
+            dataset_stats: Dataset statistics to be used for normalization. If not passed here, it is expected
+                that they will be passed with a call to `load_state_dict` before the policy is used.
+        """
+        super().__init__(config)
+
+        self.grpc_client = grpc.insecure_channel(self.config.grpc_server_address)
+        self.stub = RemotePolicyStub(self.grpc_client)
+
+        """This should be called whenever the environment is reset."""
+        if self.config.temporal_ensemble_coeff is not None:
+            self.temporal_ensembler.reset()
+        else:
+            self._action_queue = deque([], maxlen=self.config.n_action_steps)
+
+    @torch.no_grad
+    def select_action(self, batch: dict[str, Tensor]) -> Tensor:
+        self.stub.SelectAction(SelectActionRequest(batch=batch))
+
+    def forward(self, batch: dict[str, Tensor]) -> tuple[Tensor, dict]:
+        raise NotImplementedError("Remote policy does not support forward pass.")
