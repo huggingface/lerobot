45576c4d94086b84c079cbdb5acc8c398c97a9bf
Jade Choghari <chogharijade@gmail.com>
2025-04-29 22:48:39 +0300

    Update eval.py

--- lerobot/scripts/eval.py
+++ lerobot/scripts/eval.py
@@ -187,7 +187,6 @@ def rollout(
 
         # VectorEnv stores is_success in `info["final_info"][env_index]["is_success"]`. "final_info" isn't
         # available of none of the envs finished.
-        # breakpoint()
         if isinstance(env, gym.vector.VectorEnv):
             if "final_info" in info:
                 successes = [info["is_success"] if info is not None else False for info in info["final_info"]]
