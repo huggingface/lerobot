2af8edcf74b20852e736a7b06ee9564316cd7516
Steven Palma <steven.palma@huggingface.co>
2025-05-06 14:15:40 +0200

    chore(cameras): delete unused files

--- tests/cameras/test_realsense.py
+++ tests/cameras/test_realsense.py
@@ -14,6 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Example of running a specific test:
+# ```bash
+# pytest tests/cameras/test_opencv.py::test_connect
+# ```
+
 import os
 from unittest.mock import patch
 
