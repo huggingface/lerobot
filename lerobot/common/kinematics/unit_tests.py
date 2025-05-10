beae140cc9562207669409c43b5e4344c4bb23c1
Leonardo Bertelli <73482329+leo-berte@users.noreply.github.com>
2025-05-10 16:48:18 +0200

    add DH2MECH conversion (and viceversa)

--- lerobot/common/kinematics/unit_tests.py
+++ lerobot/common/kinematics/unit_tests.py
@@ -39,10 +39,19 @@ class TestKinematics(unittest.TestCase):
         q_final_dh = self.kin.inverse_kinematics(self.robot, self.q_init_dh, T_goal, use_orientation=False)
         q_final_mech = q_final_dh[:]  # self.robot.from_dh_to_mech(q_final)
         self.robot.check_joint_limits(q_final_mech)
-
+        
     def test_interpolator_delta(self):
         "test assert error when interpolation delta parameter is zero, triggering division by zero"
-        n_steps = self.kin._interp_init(self.T_start, np.eye(4), delta=0.0)
+        n_steps = self.kin._interp_init(self.T_start, np.eye(4), delta = 0.0)
+        
+    def test_dh2mechanical(self):
+        "test dh2mechanical conversion"
+        pass
+    
+    def test_mechanical2dh(self):
+        "test mechanical2dh conversion"
+        pass
+    
 
 
 if __name__ == "__main__":
