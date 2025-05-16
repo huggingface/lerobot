ca82e9aebfbf69e228baa30978954bb88ede5066
Pepijn <pepijn@huggingface.co>
2025-05-16 13:24:21 +0200

    fix for lekiwi config camera index name

--- lerobot/common/robots/lekiwi/config_lekiwi.py
+++ lerobot/common/robots/lekiwi/config_lekiwi.py
@@ -35,10 +35,10 @@ class LeKiwiConfig(RobotConfig):
     cameras: dict[str, CameraConfig] = field(
         default_factory=lambda: {
             "front": OpenCVCameraConfig(
-                camera_index="/dev/video0", fps=30, width=640, height=480, rotation=None
+                index_or_path="/dev/video0", fps=30, width=640, height=480, rotation=None
             ),
             "wrist": OpenCVCameraConfig(
-                camera_index="/dev/video2", fps=30, width=640, height=480, rotation=180
+                index_or_path="/dev/video2", fps=30, width=640, height=480, rotation=180
             ),
         }
     )
