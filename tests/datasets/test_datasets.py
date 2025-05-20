0d5411bfb07a83e9ba8d477b9db90848d309e8f1
CarolinePascal <caroline8.pascal@gmail.com>
2025-05-20 20:48:57 +0200

    test(audio frame): fixing decoded audio frame shape

--- tests/datasets/test_datasets.py
+++ tests/datasets/test_datasets.py
@@ -352,7 +352,10 @@ def test_add_frame_audio(audio_dataset):
     dataset.save_episode()
 
     assert dataset[0]["observation.audio.microphone"].shape == torch.Size(
-        (int(DEFAULT_AUDIO_CHUNK_DURATION * microphone.sample_rate), DUMMY_AUDIO_CHANNELS)
+        (
+            DUMMY_AUDIO_CHANNELS,
+            int(DEFAULT_AUDIO_CHUNK_DURATION * microphone.sample_rate),
+        )  # Match pytorch channel-first format
     )
 
 
