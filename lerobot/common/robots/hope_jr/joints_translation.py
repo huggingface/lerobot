5504b5aee252ae6cd279a1713dab9b8074ddceb3
Simon Alibert <simon.alibert@huggingface.co>
2025-05-22 11:20:38 +0200

    Fix joints translation

--- lerobot/common/robots/hope_jr/joints_translation.py
+++ lerobot/common/robots/hope_jr/joints_translation.py
@@ -24,26 +24,26 @@ def homonculus_glove_to_hope_jr_hand(glove_action: dict[str, float]) -> dict[str
         "index_ulnar_flexor.pos": get_ulnar_flexion(
             glove_action["index_mcp_flexion.pos"], glove_action["index_mcp_abduction.pos"], INDEX_SPLAY
         ),
-        "index_pip_dip.pos": glove_action["index_pip_dip.pos"],
+        "index_pip_dip.pos": glove_action["index_dip.pos"],
         "middle_radial_flexor.pos": get_radial_flexion(
             glove_action["middle_mcp_flexion.pos"], glove_action["middle_mcp_abduction.pos"], MIDDLE_SPLAY
         ),
         "middle_ulnar_flexor.pos": get_ulnar_flexion(
             glove_action["middle_mcp_flexion.pos"], glove_action["middle_mcp_abduction.pos"], MIDDLE_SPLAY
         ),
-        "middle_pip_dip.pos": glove_action["middle_pip_dip.pos"],
+        "middle_pip_dip.pos": glove_action["middle_dip.pos"],
         "ring_radial_flexor.pos": get_radial_flexion(
             glove_action["ring_mcp_flexion.pos"], glove_action["ring_mcp_abduction.pos"], RING_SPLAY
         ),
         "ring_ulnar_flexor.pos": get_ulnar_flexion(
             glove_action["ring_mcp_flexion.pos"], glove_action["ring_mcp_abduction.pos"], RING_SPLAY
         ),
-        "ring_pip_dip.pos": glove_action["ring_pip_dip.pos"],
+        "ring_pip_dip.pos": glove_action["ring_dip.pos"],
         "pinky_radial_flexor.pos": get_radial_flexion(
             glove_action["pinky_mcp_flexion.pos"], glove_action["pinky_mcp_abduction.pos"], PINKY_SPLAY
         ),
         "pinky_ulnar_flexor.pos": get_ulnar_flexion(
             glove_action["pinky_mcp_flexion.pos"], glove_action["pinky_mcp_abduction.pos"], PINKY_SPLAY
         ),
-        "pinky_pip_dip.pos": glove_action["pinky_pip_dip.pos"],
+        "pinky_pip_dip.pos": glove_action["pinky_dip.pos"],
     }
