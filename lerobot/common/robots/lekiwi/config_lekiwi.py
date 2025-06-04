01f72c55705ee4ebb599f11873c4401c4b07adeb
Pepijn <pepijn@huggingface.co>
2025-06-04 11:16:23 +0200

    fix(docs): integrated feedback pr

--- lerobot/common/robots/lekiwi/config_lekiwi.py
+++ lerobot/common/robots/lekiwi/config_lekiwi.py
@@ -36,7 +36,7 @@ class LeKiwiConfig(RobotConfig):
         default_factory=lambda: {
             "front": OpenCVCameraConfig(index_or_path="/dev/video0", fps=30, width=640, height=480),
             "wrist": OpenCVCameraConfig(
-                index_or_path="/dev/video2", fps=30, width=480, height=640, rotation=Cv2Rotation.ROTATE_90
+                index_or_path="/dev/video2", fps=30, width=640, height=480, rotation=Cv2Rotation.ROTATE_180
             ),
         }
     )
