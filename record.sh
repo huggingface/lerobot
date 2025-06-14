166e9f0c1ec4f56afd6fd1d5d53997fb4f3b4412
Jack Vial <vialjack@gmail.com>
2025-06-14 18:54:30 -0400

    video6

--- record.sh
+++ record.sh
@@ -136,7 +136,7 @@ TEST_NUMBER=$1
 python -m lerobot.record \
     --robot.type=koch_screwdriver_follower \
     --robot.port=/dev/servo_5837053138 \
-    --robot.cameras="{ screwdriver: {type: opencv, index_or_path: /dev/video0, width: 800, height: 600, fps: 30}, side: {type: opencv, index_or_path: /dev/video2, width: 800, height: 600, fps: 30}, top: {type: opencv, index_or_path: /dev/video2, width: 800, height: 600, fps: 30}}" \
+    --robot.cameras="{ screwdriver: {type: opencv, index_or_path: /dev/video0, width: 800, height: 600, fps: 30}, side: {type: opencv, index_or_path: /dev/video2, width: 800, height: 600, fps: 30}, top: {type: opencv, index_or_path: /dev/video6, width: 800, height: 600, fps: 30}}" \
     --robot.id=koch_screwdriver_follower_testing \
     --dataset.repo_id=jackvial/koch_screwdriver_follower_test_${TEST_NUMBER} \
     --dataset.num_episodes=2 \
