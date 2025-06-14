3a7233e4b2bbef1adbd8c077ae90c227d47099bb
Steven Palma <steven.palma@huggingface.co>
2025-06-14 13:56:32 +0200

    fix(record): change condition to allow both teleop and policy

--- lerobot/record.py
+++ lerobot/record.py
@@ -140,8 +140,8 @@ class RecordConfig:
     resume: bool = False
 
     def __post_init__(self):
-        if self.teleop is not None and self.policy is not None:
-            raise ValueError("Choose either a policy or a teleoperator to control the robot")
+        if self.teleop is None and self.policy is None:
+            raise ValueError("Choose a policy, a teleoperator or both to control the robot")
 
         # HACK: We parse again the cli args here to get the pretrained path if there was one.
         policy_path = parser.get_path_arg("policy")
