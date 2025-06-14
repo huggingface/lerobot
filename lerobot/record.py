0845dc0f1f1bdd4a2fb28ea6ecb6efbcce601899
Jack Vial <vialjack@gmail.com>
2025-06-14 15:55:50 -0400

    import the new robot in record

--- lerobot/record.py
+++ lerobot/record.py
@@ -58,6 +58,7 @@ from lerobot.common.robots import (  # noqa: F401
     Robot,
     RobotConfig,
     koch_follower,
+    koch_screwdriver_follower,
     make_robot_from_config,
     so100_follower,
     so101_follower,
@@ -84,7 +85,7 @@ from lerobot.common.utils.visualization_utils import _init_rerun
 from lerobot.configs import parser
 from lerobot.configs.policies import PreTrainedConfig
 
-from .common.teleoperators import koch_leader, so100_leader, so101_leader  # noqa: F401
+from .common.teleoperators import koch_leader, koch_screwdriver_leader, so100_leader, so101_leader  # noqa: F401
 
 
 @dataclass
