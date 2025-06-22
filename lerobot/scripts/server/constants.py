1beb8aa247f93c7fc3058c2ef850102f922522e1
Francesco Capuano <francesco.capuano@huggingface.co>
2025-06-22 19:04:49 +0200

    fix: minor

--- lerobot/scripts/server/constants.py
+++ lerobot/scripts/server/constants.py
@@ -1,12 +1,13 @@
 """Server/Client side: Sometimes you just want the environment to wait a tiny bit"""
 
-IDLE_WAIT = 0.01
+DEFAULT_IDLE_WAIT = 0.01
 
 """Client side: The environment evolves with a time resolution equal to environment_dt"""
-ENVIRONMENT_DT = 1 / 30
+DEFAULT_ENVIRONMENT_DT = 1 / 30
 
 """Server side: Running inference on (at most) environment_dt"""
-INFERENCE_LATENCY = ENVIRONMENT_DT
+DEFAULT_INFERENCE_LATENCY = DEFAULT_ENVIRONMENT_DT
 
 """Supported policies"""
-SUPPORTED_POLICIES = ["act", "smolvla"]
+# TODO:  Support DiffusionPolicy, VQBet and Pi0 
+supported_policies = ["act", "smolvla"]
