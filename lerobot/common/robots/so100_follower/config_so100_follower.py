34d7101ec271357fb95864f1c42fab86281358b1
David Chen <chenosaurus@gmail.com>
2025-06-25 10:12:00 -0700

    adding so100 remote follow

--- lerobot/common/robots/so100_follower/config_so100_follower.py
+++ lerobot/common/robots/so100_follower/config_so100_follower.py
@@ -16,7 +16,7 @@ from dataclasses import dataclass, field
 
 from lerobot.common.cameras import CameraConfig
 
-from ..config import RobotConfig
+from ..config import RobotConfig, RemoteRobotConfig
 
 
 @RobotConfig.register_subclass("so100_follower")
@@ -61,3 +61,20 @@ class SO100FollowerEndEffectorConfig(SO100FollowerConfig):
             "z": 0.02,
         }
     )
+
+@RemoteRobotConfig.register_subclass("so100_remote_follower")
+@dataclass
+class SO100RemoteFollowerConfig(RemoteRobotConfig):
+
+    disable_torque_on_disconnect: bool = True
+
+    # `max_relative_target` limits the magnitude of the relative positional target vector for safety purposes.
+    # Set this to a positive scalar to have the same value for all motors, or a list that is the same length as
+    # the number of motors in your follower arms.
+    max_relative_target: int | None = None
+
+    # cameras
+    cameras: dict[str, CameraConfig] = field(default_factory=dict)
+
+    # Set to `True` for backward compatibility with previous policies/dataset
+    use_degrees: bool = False
