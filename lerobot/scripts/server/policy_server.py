94105b49dc2b9e13aa8c90a4b5c874cd1e2a224a
Francesco Capuano <francesco.capuano@huggingface.co>
2025-06-27 14:29:07 +0200

    fix: unbloating logging

--- lerobot/scripts/server/policy_server.py
+++ lerobot/scripts/server/policy_server.py
@@ -9,13 +9,14 @@ import grpc
 import torch
 
 from lerobot.common.policies.factory import get_policy_class
-from lerobot.scripts.server import (
+from lerobot.common.transport import (
     async_inference_pb2,  # type: ignore
     async_inference_pb2_grpc,  # type: ignore
 )
 from lerobot.scripts.server.configs import PolicyServerConfig
 from lerobot.scripts.server.constants import supported_policies
 from lerobot.scripts.server.helpers import (
+    FPSTracker,
     TimedAction,
     TimedObservation,
     TinyPolicyConfig,
@@ -33,6 +34,9 @@ class PolicyServer(async_inference_pb2_grpc.AsyncInferenceServicer):
     def __init__(self, config: PolicyServerConfig):
         self.config = config
         self._running_event = threading.Event()
+
+        # FPS measurement
+        self.fps_tracker = FPSTracker(target_fps=config.fps)
 
         self._setup_server()
 
@@ -98,37 +102,34 @@ class PolicyServer(async_inference_pb2_grpc.AsyncInferenceServicer):
         self.logger.debug(f"Receiving observations from {client_id}")
 
         for observation in request_iterator:
-            receive_time = time.perf_counter()
+            receive_time = time.time()  # comparing timestamps so need time.time()
+            start_deserialize = time.perf_counter()
             timed_observation = pickle.loads(observation.data)  # nosec
-            deserialize_time = time.perf_counter()
+            deserialize_time = time.perf_counter() - start_deserialize
 
             self.logger.debug(f"Received observation #{timed_observation.get_timestep()}")
 
             if not self._maybe_enqueue_observation(timed_observation):
                 continue
 
-            queue_time = time.perf_counter()
-
             obs_timestep = timed_observation.get_timestep()
             obs_timestamp = timed_observation.get_timestamp()
 
+            # Calculate FPS metrics
+            fps_metrics = self.fps_tracker.calculate_fps_metrics(obs_timestamp)
+
             self.logger.info(
                 f"Received observation #{obs_timestep} | "
-                f"Client timestamp: {obs_timestamp:.6f} | "
+                f"Avg FPS: {fps_metrics['avg_fps']:.2f} | "  # fps at which observations are received from client
                 f"Server timestamp: {receive_time:.6f} | "
+                f"Client timestamp: {obs_timestamp:.6f} | "
+                f"Target: {fps_metrics['target_fps']:.2f}"
             )
-
-            if not hasattr(self, "previous_obs_timestamp"):
-                self.previous_obs_timestamp = obs_timestamp
 
             self.logger.debug(
-                f"1/DeltaObsT (~frequency): {1 / (1e-6 + obs_timestamp - self.previous_obs_timestamp):.6f} Hz| "
                 f"Network latency: {receive_time - obs_timestamp:.6f}s | "
-                f"Deserialization time: {deserialize_time - receive_time:.6f}s | "
-                f"Queue time: {queue_time - deserialize_time:.6f}s | "
+                f"Deserialization time: {deserialize_time:.6f}s"
             )
-
-            self.previous_obs_timestamp = obs_timestamp
 
         return async_inference_pb2.Empty()
 
@@ -144,36 +145,32 @@ class PolicyServer(async_inference_pb2_grpc.AsyncInferenceServicer):
                 f"Running inference for observation #{obs.get_timestep()} (must_go: {obs.must_go})"
             )
 
-            if obs:
-                self.last_predicted_obs = obs
-                self._predicted_timesteps.add(obs.get_timestep())
+            self.last_processed_obs = obs
+            self._predicted_timesteps.add(obs.get_timestep())
 
-                start_time = time.time()
-                action_chunk = self._predict_action_chunk(obs)
-                inference_time = time.time() - start_time
+            start_time = time.perf_counter()
+            action_chunk = self._predict_action_chunk(obs)
+            inference_time = time.perf_counter() - start_time
 
-                start_time = time.time()
-                action_bytes = pickle.dumps(action_chunk)  # nosec
-                serialize_time = time.perf_counter() - start_time
+            start_time = time.perf_counter()
+            action_bytes = pickle.dumps(action_chunk)  # nosec
+            serialize_time = time.perf_counter() - start_time
 
-                # Create and return the Action
-                action = async_inference_pb2.Action(transfer_state=obs.transfer_state, data=action_bytes)
+            # Create and return the Action
+            action = async_inference_pb2.Action(data=action_bytes)
 
-                self.logger.info(
-                    f"Action chunk #{obs.get_timestep()} generated | Inference time: {inference_time:.6f}s |"
-                )
+            self.logger.info(
+                f"Action chunk #{obs.get_timestep()} generated |Inference time: {inference_time:.2f}s |"
+            )
 
-                self.logger.debug(
-                    f"Action chunk #{obs.get_timestep()} generated | "
-                    f"Inference time: {inference_time:.6f}s |"
-                    f"Serialize time: {serialize_time:.6f}s |"
-                    f"Total time: {inference_time + serialize_time:.6f}s"
-                )
+            self.logger.debug(
+                f"Action chunk #{obs.get_timestep()} generated | "
+                f"Inference time: {inference_time:.2f}s |"
+                f"Serialize time: {serialize_time:.2f}s |"
+                f"Total time: {inference_time + serialize_time:.2f}s"
+            )
 
-                yield action
-            else:
-                self.logger.warning("No observation in queue yet!")
-                time.sleep(self.config.idle_wait)
+            yield action
 
         except Empty:  # no observation added to queue in obs_queue_timeout
             return async_inference_pb2.Empty()
@@ -212,12 +209,12 @@ class PolicyServer(async_inference_pb2_grpc.AsyncInferenceServicer):
         """Enqueue an observation if it must go through processing, otherwise skip it.
         Observations not in queue are never run through the policy network"""
 
-        if obs.must_go or not hasattr(self, "last_predicted_obs"):
+        if obs.must_go or not hasattr(self, "last_processed_obs"):
             self.logger.info(f"[MUST GO] Enqueued observation #{obs.get_timestep()} for direct processing!")
             return self._enqueue_and_go(obs)
 
         else:
-            if self._obs_sanity_checks(obs, self.last_predicted_obs):
+            if self._obs_sanity_checks(obs, self.last_processed_obs):
                 return self._enqueue_and_go(obs)
             else:
                 return False
@@ -228,7 +225,7 @@ class PolicyServer(async_inference_pb2_grpc.AsyncInferenceServicer):
         t_0 + i*environment_dt for i in range(len(action_chunk))
         """
         return [
-            TimedAction(t_0 + i * self.config.environment_dt, action, i_0 + i)
+            TimedAction(timestamp=t_0 + i * self.config.environment_dt, timestep=i_0 + i, action=action)
             for i, action in enumerate(action_chunk)
         ]
 
@@ -377,4 +374,4 @@ def serve(config: Optional[PolicyServerConfig] = None, host: str = "localhost", 
 
 
 if __name__ == "__main__":
-    serve()  # use default network configuration
+    serve()  # pass a
