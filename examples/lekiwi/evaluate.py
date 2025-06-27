8299dffc7e330d839b23a858ea51496b1e153b4e
Pepijn <pepijn@huggingface.co>
2025-06-27 15:32:26 +0200

    feat: Add fixes and refactor lekiwi example

--- examples/lekiwi/evaluate.py
+++ examples/lekiwi/evaluate.py
@@ -1,32 +1,60 @@
-from lerobot.common.datasets.utils import build_dataset_frame, hw_to_dataset_features
+from examples.lekiwi.utils import lekiwi_record_loop
+from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
+from lerobot.common.datasets.utils import hw_to_dataset_features
 from lerobot.common.policies.act.modeling_act import ACTPolicy
 from lerobot.common.robots.lekiwi import LeKiwiClient, LeKiwiClientConfig
-from lerobot.common.utils.control_utils import predict_action
-from lerobot.common.utils.utils import get_safe_torch_device
+from lerobot.common.utils.utils import log_say
+from lerobot.common.utils.visualization_utils import _init_rerun
 
-NB_CYCLES_CLIENT_CONNECTION = 1000
+NUM_EPISODES = 5
+FPS = 30
+EPISODE_TIME_SEC = 60
+TASK_DESCRIPTION = "My task description"
 
+# Create the robot and teleoperator configurations
 robot_config = LeKiwiClientConfig(remote_ip="172.18.134.136", id="lekiwi")
 robot = LeKiwiClient(robot_config)
 
+policy = ACTPolicy.from_pretrained("pepijn223/lekiwi_recent")
+
+# Configure the dataset features
+action_features = hw_to_dataset_features(robot.action_features, "action")
+obs_features = hw_to_dataset_features(robot.observation_features, "observation")
+dataset_features = {**action_features, **obs_features}
+
+# Create the dataset
+dataset = LeRobotDataset.create(
+    repo_id="pepijn223/eval_lekiwi_test_act5",
+    fps=FPS,
+    features=dataset_features,
+    robot_type=robot.name,
+    use_videos=True,
+    image_writer_threads=4,
+)
+
 robot.connect()
 
-policy = ACTPolicy.from_pretrained("pepijn223/act_lekiwi_circle")
-policy.reset()
+_init_rerun(session_name="recording")
 
-obs_features = hw_to_dataset_features(robot.observation_features, "observation")
+if not robot.is_connected:
+    exit()
 
-print("Running inference")
-i = 0
-while i < NB_CYCLES_CLIENT_CONNECTION:
-    obs = robot.get_observation()
+for episode_idx in range(NUM_EPISODES):
+    log_say(f"Running inference, recording eval episode {episode_idx + 1} of {NUM_EPISODES}")
 
-    observation_frame = build_dataset_frame(obs_features, obs, prefix="observation")
-    action_values = predict_action(
-        observation_frame, policy, get_safe_torch_device(policy.config.device), policy.config.use_amp
+    # Run the policy inference loop
+    lekiwi_record_loop(
+        robot=robot,
+        fps=FPS,
+        policy=policy,
+        dataset=dataset,
+        control_time_s=EPISODE_TIME_SEC,
+        single_task=TASK_DESCRIPTION,
+        log_data=True,
     )
-    action = {key: action_values[i].item() for i, key in enumerate(robot.action_features)}
-    robot.send_action(action)
-    i += 1
 
+    dataset.save_episode()
+
+# Clean up and upload to hub
 robot.disconnect()
+dataset.push_to_hub()
--- examples/lekiwi/record.py
+++ examples/lekiwi/record.py
@@ -1,67 +1,76 @@
 import time
 
+from examples.lekiwi.utils import lekiwi_record_loop
 from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
 from lerobot.common.datasets.utils import hw_to_dataset_features
 from lerobot.common.robots.lekiwi.config_lekiwi import LeKiwiClientConfig
 from lerobot.common.robots.lekiwi.lekiwi_client import LeKiwiClient
 from lerobot.common.teleoperators.keyboard import KeyboardTeleop, KeyboardTeleopConfig
 from lerobot.common.teleoperators.so100_leader import SO100Leader, SO100LeaderConfig
+from lerobot.common.utils.utils import log_say
+from lerobot.common.utils.visualization_utils import _init_rerun
 
-NB_CYCLES_CLIENT_CONNECTION = 250
+NUM_EPISODES = 1
+FPS = 30
+EPISODE_TIME_SEC = 30
+RESET_TIME_SEC = 10
+TASK_DESCRIPTION = "My task description"
 
-leader_arm_config = SO100LeaderConfig(port="/dev/tty.usbmodem58760431551")
-leader_arm = SO100Leader(leader_arm_config)
-
+# Create the robot and teleoperator configurations
+robot_config = LeKiwiClientConfig(remote_ip="172.18.134.136", id="lekiwi")
+leader_arm_config = SO100LeaderConfig(port="/dev/tty.usbmodem585A0077581", id="my_awesome_leader_arm")
 keyboard_config = KeyboardTeleopConfig()
+
+robot = LeKiwiClient(robot_config)
+leader_arm = SO100Leader(leader_arm_config)
 keyboard = KeyboardTeleop(keyboard_config)
 
-robot_config = LeKiwiClientConfig(remote_ip="172.18.134.136", id="lekiwi")
-robot = LeKiwiClient(robot_config)
-
+# Configure the dataset features
 action_features = hw_to_dataset_features(robot.action_features, "action")
 obs_features = hw_to_dataset_features(robot.observation_features, "observation")
 dataset_features = {**action_features, **obs_features}
 
+# Create the dataset
 dataset = LeRobotDataset.create(
     repo_id="pepijn223/lekiwi" + str(int(time.time())),
-    fps=10,
+    fps=FPS,
     features=dataset_features,
     robot_type=robot.name,
+    use_videos=True,
+    image_writer_threads=4,
 )
 
 leader_arm.connect()
 keyboard.connect()
 robot.connect()
 
+_init_rerun(session_name="lekiwi_record")
+
 if not robot.is_connected or not leader_arm.is_connected or not keyboard.is_connected:
     exit()
 
-print("Starting LeKiwi recording")
-i = 0
-while i < NB_CYCLES_CLIENT_CONNECTION:
-    arm_action = leader_arm.get_action()
-    arm_action = {f"arm_{k}": v for k, v in arm_action.items()}
+for episode_idx in range(NUM_EPISODES):
+    log_say(f"Recording episode {episode_idx + 1} of {NUM_EPISODES}")
 
-    keyboard_keys = keyboard.get_action()
+    # Run the record loop
+    lekiwi_record_loop(
+        robot=robot,
+        fps=FPS,
+        teleop_arm=leader_arm,
+        teleop_keyboard=keyboard,
+        dataset=dataset,
+        control_time_s=EPISODE_TIME_SEC,
+        single_task=TASK_DESCRIPTION,
+        log_data=True,
+    )
 
-    base_action = robot._from_keyboard_to_base_action(keyboard_keys)
+    log_say("Reset environment")
+    time.sleep(int(RESET_TIME_SEC))
 
-    action = {**arm_action, **base_action} if len(base_action) > 0 else arm_action
+# Clean up and upload to hub
+dataset.save_episode()
+dataset.push_to_hub()
 
-    action_sent = robot.send_action(action)
-    observation = robot.get_observation()
-
-    frame = {**action_sent, **observation}
-    task = "Dummy Example Task Dataset"
-
-    dataset.add_frame(frame, task)
-    i += 1
-
-print("Disconnecting Teleop Devices and LeKiwi Client")
 robot.disconnect()
 leader_arm.disconnect()
 keyboard.disconnect()
-
-print("Uploading dataset to the hub")
-dataset.save_episode()
-dataset.push_to_hub()
