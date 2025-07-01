5de3617ce9ba415e243e2e61474353515487b15f
Simon Alibert <simon.alibert@huggingface.co>
2025-07-01 13:32:10 +0200

    Downgrade grpc-tools

--- src/lerobot/transport/services_pb2_grpc.py
+++ src/lerobot/transport/services_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from src.lerobot.transport import services_pb2 as src_dot_lerobot_dot_transport_dot_services__pb2
 
-GRPC_GENERATED_VERSION = '1.73.1'
+GRPC_GENERATED_VERSION = '1.71.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
