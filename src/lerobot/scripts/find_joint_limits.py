63ddfefa0806156433c449237573c383cb809922
Michel Aractingi <michel.aractingi@huggingface.co>
2025-07-02 18:08:20 +0200

    Remove references to lerobot.common (#1432)

--- src/lerobot/scripts/find_joint_limits.py
+++ src/lerobot/scripts/find_joint_limits.py
@@ -36,7 +36,6 @@ from dataclasses import dataclass
 import draccus
 import numpy as np
 
-from lerobot.common.utils.robot_utils import busy_wait
 from lerobot.model.kinematics import RobotKinematics
 from lerobot.robots import (  # noqa: F401
     RobotConfig,
@@ -51,6 +50,7 @@ from lerobot.teleoperators import (  # noqa: F401
     make_teleoperator_from_config,
     so100_leader,
 )
+from lerobot.utils.robot_utils import busy_wait
 
 
 @dataclass
