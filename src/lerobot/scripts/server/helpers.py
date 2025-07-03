54b76ec8d8808e9a5771107919930f6f6f6b08c8
Francesco Capuano <francesco.capuano@huggingface.co>
2025-07-03 12:11:56 +0200

    fix: move dep in function

--- src/lerobot/scripts/server/helpers.py
+++ src/lerobot/scripts/server/helpers.py
@@ -10,7 +10,6 @@ from pathlib import Path
 from threading import Event
 from typing import Any
 
-import matplotlib.pyplot as plt
 import torch
 
 from lerobot.cameras.opencv.configuration_opencv import OpenCVCameraConfig
@@ -46,6 +45,8 @@ Observation = dict[str, torch.Tensor]
 
 
 def visualize_action_queue_size(action_queue_size: list[int]) -> None:
+    import matplotlib.pyplot as plt
+
     fig, ax = plt.subplots()
     ax.set_title("Action Queue Size Over Time")
     ax.set_xlabel("Environment steps")
