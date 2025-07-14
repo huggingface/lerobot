c5b5090166c56fb1d879a32ed427584edade8aec
Ben Zhang <benzhangniu@gmail.com>
2025-07-13 19:03:13 -0700

    Add pi0fast to async policies

--- src/lerobot/scripts/server/helpers.py
+++ src/lerobot/scripts/server/helpers.py
@@ -28,7 +28,7 @@ from lerobot.constants import OBS_IMAGES, OBS_STATE
 from lerobot.datasets.utils import build_dataset_frame, hw_to_dataset_features
 
 # NOTE: Configs need to be loaded for the client to be able to instantiate the policy config
-from lerobot.policies import ACTConfig, DiffusionConfig, PI0Config, SmolVLAConfig, VQBeTConfig  # noqa: F401
+from lerobot.policies import ACTConfig, DiffusionConfig, PI0Config, PI0FASTConfig, SmolVLAConfig, VQBeTConfig  # noqa: F401
 from lerobot.robots.robot import Robot
 from lerobot.utils.utils import init_logging
 
