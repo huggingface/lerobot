0938a1d816c70d689ab6df3298a71572798c15a6
Pepijn <138571049+pkooij@users.noreply.github.com>
2025-07-16 17:50:36 +0200

    Feat/add bimanual so100 robot (#1509)

--- src/lerobot/teleoperators/utils.py
+++ src/lerobot/teleoperators/utils.py
@@ -61,5 +61,9 @@ def make_teleoperator_from_config(config: TeleoperatorConfig) -> Teleoperator:
         from .homunculus import HomunculusArm
 
         return HomunculusArm(config)
+    elif config.type == "bi_so100_leader":
+        from .bi_so100_leader import BiSO100Leader
+
+        return BiSO100Leader(config)
     else:
         raise ValueError(config.type)
