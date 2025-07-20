910aea792cb04c83cea6fe231b124c2eb43cad56
Steven Palma <steven.palma@huggingface.co>
2025-07-20 23:14:33 +0200

    chore(teleop): consistency of calibration check in koch

--- src/lerobot/teleoperators/koch_leader/koch_leader.py
+++ src/lerobot/teleoperators/koch_leader/koch_leader.py
@@ -75,7 +75,10 @@ class KochLeader(Teleoperator):
 
         self.bus.connect()
         if not self.is_calibrated and calibrate:
-            self.calibrate(calibration=self.calibration)
+            logger.info(
+                "Mismatch between calibration values in the motor and the calibration file or no calibration file found"
+            )
+            self.calibrate()
 
         self.configure()
         logger.info(f"{self} connected.")
@@ -84,16 +87,15 @@ class KochLeader(Teleoperator):
     def is_calibrated(self) -> bool:
         return self.bus.is_calibrated
 
-    def calibrate(self, calibration: dict[str, MotorCalibration] | None = None) -> None:
-        if calibration is not None and calibration:
-            logging.info("Mismatch between calibration values in the motor and the calibration file")
-
+    def calibrate(self) -> None:
+        if self.calibration:
+            # Calibration file exists, ask user whether to use it or run new calibration
             user_input = input(
                 f"Press ENTER to use provided calibration file associated with the id {self.id}, or type 'c' and press ENTER to run calibration: "
             )
             if user_input.strip().lower() != "c":
-                logging.info(f"Using provided calibration file associated with the id {self.id}")
-                self.bus.write_calibration(calibration)
+                logger.info(f"Writing calibration file associated with the id {self.id} to the motors")
+                self.bus.write_calibration(self.calibration)
                 return
         logger.info(f"\nRunning calibration of {self}")
         self.bus.disable_torque()
