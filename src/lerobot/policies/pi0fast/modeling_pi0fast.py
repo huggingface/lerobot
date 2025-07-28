eb251b41c72e39cd68e2a811c9060d3eb7fa000f
Adil Zouitine <adilzouitinegm@gmail.com>
2025-07-28 11:49:05 +0200

    chore(pi0fast): TODO comment to warn the need for removal ignore_index (#1593)

--- src/lerobot/policies/pi0fast/modeling_pi0fast.py
+++ src/lerobot/policies/pi0fast/modeling_pi0fast.py
@@ -488,6 +488,8 @@ class PI0FAST(nn.Module):
                 param.data = param.data.to(dtype=torch_precision)
         self.set_requires_grad()
         self.image_keys = self.config.image_features.keys()
+        # TODO: Remove this once we bump transformers to >4.52.0 because the attribute will be removed
+        # AttributeError: 'PaliGemmaConfig' object has no attribute 'ignore_index'
         self.ignore_index = self.pi0_paligemma.config.ignore_index
         self.padding_side = self.config.padding_side
 
