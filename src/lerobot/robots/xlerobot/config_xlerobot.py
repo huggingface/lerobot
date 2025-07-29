0d4b342be75eaccf883bb950babb8b0c8768aa7d
Vector-Wangel <vector200046@hotmail.com>
2025-07-29 15:12:42 -0500

    xlerobot

--- src/lerobot/robots/xlerobot/config_xlerobot.py
+++ src/lerobot/robots/xlerobot/config_xlerobot.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-
-# Copyright 2025 The HuggingFace Inc. team. All rights reserved.
+# Copyright 2024 The HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,37 +14,117 @@
 
 from dataclasses import dataclass, field
 
-from lerobot.cameras import CameraConfig
+from lerobot.cameras.configs import CameraConfig, Cv2Rotation, ColorMode
+from lerobot.cameras.opencv.configuration_opencv import OpenCVCameraConfig
+from lerobot.cameras.realsense import RealSenseCamera, RealSenseCameraConfig
 
 from ..config import RobotConfig
 
 
+def xlerobot_cameras_config() -> dict[str, CameraConfig]:
+    return {
+        # "left_wrist": OpenCVCameraConfig(
+        #     index_or_path="/dev/video0", fps=30, width=640, height=480, rotation=Cv2Rotation.NO_ROTATION
+        # ),
+
+        # "right_wrist": OpenCVCameraConfig(
+        #     index_or_path="/dev/video2", fps=30, width=640, height=480, rotation=Cv2Rotation.NO_ROTATION
+        # ),  
+
+        # "head(RGDB)": OpenCVCameraConfig(
+        #     index_or_path="/dev/video5", fps=30, width=640, height=480, rotation=Cv2Rotation.NO_ROTATION
+        # ),                     
+        
+        # "head": RealSenseCameraConfig(
+        #     serial_number_or_name="125322060037",  # Replace with camera SN
+        #     fps=30,
+        #     width=1280,
+        #     height=720,
+        #     color_mode=ColorMode.BGR, # Request BGR output
+        #     rotation=Cv2Rotation.NO_ROTATION,
+        #     use_depth=True
+        # ),
+    }
+
+
 @RobotConfig.register_subclass("xlerobot")
 @dataclass
-class XLeRobotConfig(RobotConfig):
-    left_arm_port: str
-    right_arm_port: str
-    head_port: str
-    base_port: str
+class XLerobotConfig(RobotConfig):
+    
+    port1: str = "/dev/ttyACM0"  # port to connect to the bus (so101 + head camera)
+    port2: str = "/dev/ttyACM1"  # port to connect to the bus (same as lekiwi setup)
+    disable_torque_on_disconnect: bool = True
 
-    # Left arm (SO100Follower + head joints)
-    left_arm_disable_torque_on_disconnect: bool = True
-    left_arm_max_relative_target: int | None = None
-    left_arm_use_degrees: bool = False
+    # `max_relative_target` limits the magnitude of the relative positional target vector for safety purposes.
+    # Set this to a positive scalar to have the same value for all motors, or a list that is the same length as
+    # the number of motors in your follower arms.
+    max_relative_target: int | None = None
 
-    # Right arm (SO100Follower + base motors)
-    right_arm_disable_torque_on_disconnect: bool = True
-    right_arm_max_relative_target: int | None = None
-    right_arm_use_degrees: bool = False
+    cameras: dict[str, CameraConfig] = field(default_factory=xlerobot_cameras_config)
 
-    # Head joints (positional control)
-    head_disable_torque_on_disconnect: bool = True
-    head_max_relative_target: int | None = None
-    head_use_degrees: bool = False
+    # Set to `True` for backward compatibility with previous policies/dataset
+    use_degrees: bool = False
 
-    # Base motors (velocity control)
-    base_disable_torque_on_disconnect: bool = True
-    base_use_degrees: bool = False
+    teleop_keys: dict[str, str] = field(
+        default_factory=lambda: {
+            # Movement
+            "forward": "i",
+            "backward": "k",
+            "left": "j",
+            "right": "l",
+            "rotate_left": "u",
+            "rotate_right": "o",
+            # Speed control
+            "speed_up": "n",
+            "speed_down": "m",
+            # quit teleop
+            "quit": "b",
+        }
+    )
 
-    # cameras (shared between both arms)
-    cameras: dict[str, CameraConfig] = field(default_factory=dict)
+
+
+@dataclass
+class XLerobotHostConfig:
+    # Network Configuration
+    port_zmq_cmd: int = 5555
+    port_zmq_observations: int = 5556
+
+    # Duration of the application
+    connection_time_s: int = 3600
+
+    # Watchdog: stop the robot if no command is received for over 0.5 seconds.
+    watchdog_timeout_ms: int = 500
+
+    # If robot jitters decrease the frequency and monitor cpu load with `top` in cmd
+    max_loop_freq_hz: int = 30
+
+@RobotConfig.register_subclass("xlerobot_client")
+@dataclass
+class XLerobotClientConfig(RobotConfig):
+    # Network Configuration
+    remote_ip: str
+    port_zmq_cmd: int = 5555
+    port_zmq_observations: int = 5556
+
+    teleop_keys: dict[str, str] = field(
+        default_factory=lambda: {
+            # Movement
+            "forward": "i",
+            "backward": "k",
+            "left": "j",
+            "right": "l",
+            "rotate_left": "u",
+            "rotate_right": "o",
+            # Speed control
+            "speed_up": "n",
+            "speed_down": "m",
+            # quit teleop
+            "quit": "b",
+        }
+    )
+
+    cameras: dict[str, CameraConfig] = field(default_factory=xlerobot_cameras_config)
+
+    polling_timeout_ms: int = 15
+    connect_timeout_s: int = 5
