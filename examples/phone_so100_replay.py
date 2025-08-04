a74f7f6975b9c3826f3a8dcb93c0f258a82326c6
Pepijn <pepijn@huggingface.co>
2025-08-04 19:48:50 +0200

    Not code

--- examples/phone_so100_replay.py
+++ examples/phone_so100_replay.py
@@ -6,7 +6,7 @@ from lerobot.processor.pipeline import RobotProcessor
 from lerobot.processor.utils import to_output_robot_action
 from lerobot.robots.so100_follower.config_so100_follower import SO100FollowerConfig
 from lerobot.robots.so100_follower.robot_kinematic_processor import (
-    GripperVelocityToJoint,
+    AddRobotObservationAsComplimentaryData,
     InverseKinematicsEEToJoints,
 )
 from lerobot.robots.so100_follower.so100_follower import SO100Follower
@@ -15,12 +15,12 @@ from lerobot.utils.utils import log_say
 
 episode_idx = 0
 
-robot_config = SO100FollowerConfig(port="/dev/tty.usbmodem58760434471", id="my_awesome_follower_arm")
+robot_config = SO100FollowerConfig(port="/dev/tty.usbmodem58760434471", id="my_phone_teleop_follower_arm")
 
 robot = SO100Follower(robot_config)
 robot.connect()
 
-dataset = LeRobotDataset("<hf_username>/<dataset_repo_id>", episodes=[episode_idx])
+dataset = LeRobotDataset("pepijn223/phone_pipeline_pickup1", episodes=[episode_idx])
 actions = dataset.hf_dataset.select_columns("action")
 
 # NOTE: It is highly recommended to use the urdf in the SO-ARM100 repo: https://github.com/TheRobotStudio/SO-ARM100/blob/main/Simulation/SO101/so101_new_calib.urdf
@@ -30,20 +30,35 @@ kinematics_solver = RobotKinematics(
 
 
 def to_transition_from_action(action: dict):
-    """Build an EnvTransition for the pipeline"""
-    return (None, action, None, None, None, {}, {})
+    act = {}
+
+    # EE pose
+    for k in ("ee.x", "ee.y", "ee.z", "ee.wx", "ee.wy", "ee.wz"):
+        if k in action:
+            act[f"action.{k}"] = float(action[k])
+
+    # Gripper: your dataset has absolute position
+    if "gripper.pos" in action:
+        act["action.gripper.pos"] = float(action["gripper.pos"])
+
+    return {
+        "observation": None,
+        "action": act,
+        "reward": None,
+        "done": False,
+        "truncated": False,
+        "info": {},
+        "complementary_data": {},
+    }
 
 
 # Build pipeline to convert ee pose action to joint action
 robot_ee_to_joints = RobotProcessor(
     steps=[
+        AddRobotObservationAsComplimentaryData(robot=robot),
         InverseKinematicsEEToJoints(
             kinematics=kinematics_solver,
             motor_names=list(robot.bus.motors.keys()),
-        ),
-        GripperVelocityToJoint(
-            motor_names=list(robot.bus.motors.keys()),
-            speed_factor=20.0,
         ),
     ],
     to_transition=to_transition_from_action,
@@ -54,13 +69,16 @@ log_say(f"Replaying episode {episode_idx}")
 for idx in range(dataset.num_frames):
     t0 = time.perf_counter()
 
-    action = {
+    ee_action = {
         name: float(actions[idx]["action"][i]) for i, name in enumerate(dataset.features["action"]["names"])
     }
 
-    joints_transition = robot_ee_to_joints(action)
-    robot_action_to_send = robot_ee_to_joints.to_output(joints_transition)
-    robot.send_action(robot_action_to_send)
+    print("ee_action", ee_action)
+
+    joint_action = robot_ee_to_joints(ee_action)
+    action_sent = robot.send_action(joint_action)
+
+    print("action_sent", action_sent)
 
     busy_wait(1.0 / dataset.fps - (time.perf_counter() - t0))
 
