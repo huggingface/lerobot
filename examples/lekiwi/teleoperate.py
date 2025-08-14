22875bb5c80d4b236e7e86c2c569d9107350fec1
LiuTao <liu_tao@aliyun.com>
2025-08-14 20:33:28 +0800

    1、修正windows系统下面无法打开摄像头的问题，使用DSSHOW来抓拍。 2、修正windows下面训练时无法使用torchcodec的问题，如果又不想使用pyav，可以使用decord来代替，可以开多线程，效率更高。 1. Fixed an issue where the webcam could not be opened on Windows. Use DSSHOW to capture images. 2. Fixed an issue where torchcodec could not be used during training on Windows. If you prefer not to use pyav, use decord instead, which allows for multithreading and higher efficiency.

--- examples/lekiwi/teleoperate.py
+++ examples/lekiwi/teleoperate.py
@@ -2,19 +2,20 @@ import time
 
 from lerobot.robots.lekiwi import LeKiwiClient, LeKiwiClientConfig
 from lerobot.teleoperators.keyboard.teleop_keyboard import KeyboardTeleop, KeyboardTeleopConfig
-from lerobot.teleoperators.so100_leader import SO100Leader, SO100LeaderConfig
+from lerobot.teleoperators.so101_leader import SO101Leader, SO101LeaderConfig
 from lerobot.utils.robot_utils import busy_wait
 from lerobot.utils.visualization_utils import _init_rerun, log_rerun_data
 
 FPS = 30
 
 # Create the robot and teleoperator configurations
-robot_config = LeKiwiClientConfig(remote_ip="172.18.134.136", id="my_lekiwi")
-teleop_arm_config = SO100LeaderConfig(port="/dev/tty.usbmodem585A0077581", id="my_awesome_leader_arm")
+robot_config = LeKiwiClientConfig(remote_ip="192.168.31.166", id="MyKiwi")
+# robot_config = LeKiwiClientConfig(remote_ip="100.96.43.20", id="MyKiwi")
+teleop_arm_config = SO101LeaderConfig(port="COM6", id="my_awesome_leader_arm")
 keyboard_config = KeyboardTeleopConfig(id="my_laptop_keyboard")
 
 robot = LeKiwiClient(robot_config)
-leader_arm = SO100Leader(teleop_arm_config)
+leader_arm = SO101Leader(teleop_arm_config)
 keyboard = KeyboardTeleop(keyboard_config)
 
 # To connect you already should have this script running on LeKiwi: `python -m lerobot.robots.lekiwi.lekiwi_host --robot.id=my_awesome_kiwi`
