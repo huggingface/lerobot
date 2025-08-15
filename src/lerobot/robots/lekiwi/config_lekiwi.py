0ba616ab875724b8e9c52d1d9665c19fb6eac8a8
nz-nz <nikolay.zhukovets@gmail.com>
2025-08-14 22:48:32 -0600

    update camera configuration for lekiwi

--- src/lerobot/robots/lekiwi/config_lekiwi.py
+++ src/lerobot/robots/lekiwi/config_lekiwi.py
@@ -22,12 +22,17 @@ from ..config import RobotConfig
 
 def lekiwi_cameras_config() -> dict[str, CameraConfig]:
     return {
-        "front": OpenCVCameraConfig(
-            index_or_path="/dev/video0", fps=30, width=640, height=480, rotation=Cv2Rotation.ROTATE_180
-        ),
-        "wrist": OpenCVCameraConfig(
-            index_or_path="/dev/video2", fps=30, width=480, height=640, rotation=Cv2Rotation.ROTATE_90
-        ),
+        "base.front": OpenCVCameraConfig(
+            index_or_path="/dev/video6", fps=30, width=640, height=480),
+        "top.right.wrist": OpenCVCameraConfig(
+            index_or_path="/dev/video4", fps=30, width=640, height=480),
+        "top.front": OpenCVCameraConfig(
+            index_or_path="/dev/video0", fps=30, width=640, height=480),
+        "top.left.wrist": OpenCVCameraConfig(
+            index_or_path="/dev/video2", fps=30, width=640, height=480),
+        # "top.back": OpenCVCameraConfig(
+        #     index_or_path="/dev/video8", fps=30, width=640, height=480, rotation=Cv2Rotation.ROTATE_180
+        # ),
     }
 
 
