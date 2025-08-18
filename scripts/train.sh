4c56f341ad03e2feb0876832eb3ea45f95537dab
Tianchong Jiang <tianchongjiang@gmail.com>
2025-08-18 19:30:02 +0000

    wandb name based on username

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,12 +1,12 @@
-export WANDB_API_KEY=$(cat /ripl/data/projects/lerobot/key.txt)
+export WANDB_API_KEY=$(cat /ripl/data/projects/lerobot/${USER}_key.env)
 export PYTHONWARNINGS="ignore::UserWarning:torchvision.io._video_deprecation_warning"
 python src/lerobot/scripts/train.py \
   --dataset.repo_id=so101_pick_50 \
   --dataset.root=/ripl/data/projects/lerobot/datasets/so101_pick_50 \
   --dataset.video_backend=pyav \
   --policy.type=act \
-  --output_dir=/ripl/data/projects/lerobot/outputs/train/aug18_pick_50_no_norm \
-  --job_name=aug18_pick_50_no_norm \
+  --output_dir=/ripl/data/projects/lerobot/outputs/train/test \
+  --job_name=test \
   --policy.device=cuda \
   --policy.push_to_hub=false \
   --wandb.project=lerobot \
