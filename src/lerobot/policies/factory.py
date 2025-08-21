a8235d8e5355d9da1864ea84578fb90e6a0d3ea8
Maximilian Li <max@pokeandwiggle.com>
2025-08-21 18:45:40 +0200

    fix(factory): ensure output and input features are set only if not already defined

--- src/lerobot/policies/factory.py
+++ src/lerobot/policies/factory.py
@@ -157,8 +157,10 @@ def make_policy(
             )
         features = env_to_policy_features(env_cfg)
 
-    cfg.output_features = {key: ft for key, ft in features.items() if ft.type is FeatureType.ACTION}
-    cfg.input_features = {key: ft for key, ft in features.items() if key not in cfg.output_features}
+    if not cfg.output_features:
+        cfg.output_features = {key: ft for key, ft in features.items() if ft.type is FeatureType.ACTION}
+    if not cfg.input_features:
+        cfg.input_features = {key: ft for key, ft in features.items() if key not in cfg.output_features}
     kwargs["config"] = cfg
 
     if cfg.pretrained_path:
