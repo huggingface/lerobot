d27bb9fc51244d1efeca0147be55598d61c6ab57
Skyler <skylerwiernik@gmail.com>
2025-08-23 07:21:50 -0400

    fix(#1779): Add missing imports for so101 leader/follower

--- src/lerobot/scripts/find_joint_limits.py
+++ src/lerobot/scripts/find_joint_limits.py
@@ -42,6 +42,7 @@ from lerobot.robots import (  # noqa: F401
     koch_follower,
     make_robot_from_config,
     so100_follower,
+    so101_follower
 )
 from lerobot.teleoperators import (  # noqa: F401
     TeleoperatorConfig,
@@ -49,6 +50,7 @@ from lerobot.teleoperators import (  # noqa: F401
     koch_leader,
     make_teleoperator_from_config,
     so100_leader,
+    so101_leader
 )
 from lerobot.utils.robot_utils import busy_wait
 
