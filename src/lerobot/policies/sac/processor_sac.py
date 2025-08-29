0ed900714c81fd3b54f84eabcbe4db970cc3feda
Adil Zouitine <adilzouitinegm@gmail.com>
2025-08-29 16:36:10 +0200

    refactor(pipeline): introduce RobotProcessorPipeline and PolicyProcessorPipeline as type aliases for DataProcessorPipeline (#1804)
    * refactor(pipeline): introduce RobotProcessorPipeline and PolicyProcessorPipeline as type aliases for DataProcessorPipeline
    
    * refactor(pipeline): replace DataProcessorPipeline with PolicyProcessorPipeline alias across multiple policy files for consistency
    
    * refactor(pipeline): update references to DataProcessorPipeline with RobotProcessorPipeline in record, replay, and teleoperate modules for improved consistency

--- src/lerobot/policies/sac/processor_sac.py
+++ src/lerobot/policies/sac/processor_sac.py
@@ -20,9 +20,9 @@ import torch
 from lerobot.constants import POSTPROCESSOR_DEFAULT_NAME, PREPROCESSOR_DEFAULT_NAME
 from lerobot.policies.sac.configuration_sac import SACConfig
 from lerobot.processor import (
-    DataProcessorPipeline,
     DeviceProcessor,
     NormalizerProcessor,
+    PolicyProcessorPipeline,
     RenameProcessor,
     ToBatchProcessor,
     UnnormalizerProcessor,
@@ -31,7 +31,7 @@ from lerobot.processor import (
 
 def make_sac_pre_post_processors(
     config: SACConfig, dataset_stats: dict[str, dict[str, torch.Tensor]] | None = None
-) -> tuple[DataProcessorPipeline, DataProcessorPipeline]:
+) -> tuple[PolicyProcessorPipeline, PolicyProcessorPipeline]:
     input_steps = [
         RenameProcessor(rename_map={}),
         NormalizerProcessor(
@@ -48,6 +48,6 @@ def make_sac_pre_post_processors(
             features=config.output_features, norm_map=config.normalization_mapping, stats=dataset_stats
         ),
     ]
-    return DataProcessorPipeline(steps=input_steps, name=PREPROCESSOR_DEFAULT_NAME), DataProcessorPipeline(
-        steps=output_steps, name=POSTPROCESSOR_DEFAULT_NAME
-    )
+    return PolicyProcessorPipeline(
+        steps=input_steps, name=PREPROCESSOR_DEFAULT_NAME
+    ), PolicyProcessorPipeline(steps=output_steps, name=POSTPROCESSOR_DEFAULT_NAME)
