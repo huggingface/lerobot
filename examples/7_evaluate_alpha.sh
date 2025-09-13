3387236dd26639ac45f8d123c2dd17af7bd6eb0a
Jade Choghari <chogharijade@gmail.com>
2025-09-13 18:06:55 +0200

    remove files

--- examples/7_evaluate_alpha.sh
+++
@@ -1,9 +0,0 @@
-export MUJOCO_GL=egl
-python src/lerobot/scripts/eval.py \
-    --policy.path=/raid/jade/models/act_aloha \
-    --output_dir=outputs/eval/act_aloha_transfer/080000 \
-    --env.type=aloha \
-    --env.task=AlohaTransferCube-v0 \
-    --eval.n_episodes=1 \
-    --eval.batch_size=1 \
-    --policy.use_amp=false
