1a4a47e804691972c10fb153b5593bf01fe514fa
Jade Choghari <chogharijade@gmail.com>
2025-09-16 12:58:56 +0200

    Change max_parallel_tasks from 5 to 1
    Signed-off-by: Jade Choghari <chogharijade@gmail.com>

--- src/lerobot/envs/configs.py
+++ src/lerobot/envs/configs.py
@@ -30,7 +30,7 @@ class EnvConfig(draccus.ChoiceRegistry, abc.ABC):
     fps: int = 30
     features: dict[str, PolicyFeature] = field(default_factory=dict)
     features_map: dict[str, str] = field(default_factory=dict)
-    max_parallel_tasks: int = 5
+    max_parallel_tasks: int = 1
 
     @property
     def type(self) -> str:
