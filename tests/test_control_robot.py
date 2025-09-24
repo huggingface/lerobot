7359e18eb65e33649951c5f7c959cc8fc8cf2ab2
Steven Palma <imstevenpmwork@ieee.org>
2025-09-24 14:48:23 +0200

    chore(scripts): move replay to scripts (#2021)
    Signed-off-by: Steven Palma <imstevenpmwork@ieee.org>

--- tests/test_control_robot.py
+++ tests/test_control_robot.py
@@ -16,9 +16,9 @@
 
 from unittest.mock import patch
 
-from lerobot.replay import DatasetReplayConfig, ReplayConfig, replay
 from lerobot.scripts.lerobot_calibrate import CalibrateConfig, calibrate
 from lerobot.scripts.lerobot_record import DatasetRecordConfig, RecordConfig, record
+from lerobot.scripts.lerobot_replay import DatasetReplayConfig, ReplayConfig, replay
 from lerobot.scripts.lerobot_teleoperate import TeleoperateConfig, teleoperate
 from tests.fixtures.constants import DUMMY_REPO_ID
 from tests.mocks.mock_robot import MockRobotConfig
