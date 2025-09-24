6ca2a13179f9b531fbf95e0949cdf1ccc9e855ca
Pepijn <pepijn@huggingface.co>
2025-09-24 17:23:08 +0200

    add headers pi

--- src/lerobot/policies/pi0/processor_pi0.py
+++ src/lerobot/policies/pi0/processor_pi0.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Copyright 2025 Physical Intelligence and The HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
