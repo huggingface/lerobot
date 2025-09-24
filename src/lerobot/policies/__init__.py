01efc83fef992f347247bb4c41c663ccf3147089
Pepijn <pepijn@huggingface.co>
2025-09-24 15:33:59 +0200

    fix import

--- src/lerobot/policies/__init__.py
+++ src/lerobot/policies/__init__.py
@@ -14,7 +14,7 @@
 
 from .act.configuration_act import ACTConfig as ACTConfig
 from .diffusion.configuration_diffusion import DiffusionConfig as DiffusionConfig
-from .pi0.configuration_pi0 import PI0Config
+from .pi0.configuration_pi0 import PI0Config as PI0Config
 from .pi05.configuration_pi05 import PI05Config as PI05Config
 from .smolvla.configuration_smolvla import SmolVLAConfig as SmolVLAConfig
 from .smolvla.processor_smolvla import SmolVLANewLineProcessor
