fix rtc signs

examples/rtc/evaluate_rtc_on_dataset.py
@@ -45,6 +45,7 @@ from pathlib import Path
 import numpy as np
 import torch
 from torch import Tensor
+import matplotlib.pyplot as plt
 
 from lerobot.configs import parser
 from lerobot.configs.default import DatasetConfig
@@ -267,7 +268,7 @@ class RTCDatasetEvaluator:
 
         # Load dataset
         logger.info(f"Loading dataset: {cfg.dataset.repo_id}")
-        self.dataset = LeRobotDataset(cfg.dataset.repo_id)
+        self.dataset = LeRobotDataset(cfg.dataset.repo_id, delta_timestamps={"action": np.arange(50) / 30})
         logger.info(f"Dataset loaded: {len(self.dataset)} samples, {self.dataset.num_episodes} episodes")
 
         # Create preprocessor/postprocessor
@@ -298,8 +299,8 @@ class RTCDatasetEvaluator:
         dataloader = torch.utils.data.DataLoader(
             self.dataset,
             batch_size=batch_size,
-            shuffle=False,
-            num_workers=4,
+            shuffle=True,
+            num_workers=0,
         )
 
         inference_times = 0
@@ -328,6 +329,7 @@ class RTCDatasetEvaluator:
             if prev_actions_chunk is not None:
                 # Get remaining actions from previous chunk (skip executed actions)
                 prev_chunk_left_over = prev_actions_chunk[:, last_inference_i:]
+            prev_chunk_left_over = preprocessed_batch["action"][0, :, :25]
 
             # Generate actions WITHOUT RTC
             self.policy.config.rtc_config.enabled = False
@@ -342,7 +344,7 @@ class RTCDatasetEvaluator:
             # Generate actions WITH RTC
             self.policy.config.rtc_config.enabled = True
             with torch.no_grad():
-                print(prev_chunk_left_over)
+                # print(prev_chunk_left_over)
                 rtc_actions = self.policy.predict_action_chunk(
                     preprocessed_batch,
                     noise=noise_clone,
@@ -351,7 +353,22 @@ class RTCDatasetEvaluator:
                     execution_horizon=self.cfg.rtc.execution_horizon,
                 )
 
-            self.compare_actions(rtc_actions, no_rtc_actions, inference_delay, prev_chunk_left_over, self.cfg.rtc.execution_horizon)
+            _, axs = plt.subplots(6, figsize=(15, 10))
+            self.axs = axs.T.flatten()
+            self.plot_waypoints(prev_chunk_left_over[0].cpu().numpy(), label="gt", color="green")
+            self.plot_waypoints(rtc_actions[0].cpu().numpy(), label="rtc", color="red")
+            self.plot_waypoints(no_rtc_actions[0].cpu().numpy(), label="no_rtc", color="blue")
+            plt.savefig("plot.png")
+
+            self.compare_actions(
+                rtc_actions,
+                no_rtc_actions,
+                inference_delay,
+                prev_chunk_left_over,
+                self.cfg.rtc.execution_horizon,
+            )
+
+            break
 
             prev_actions_chunk = rtc_actions
             inference_times += 1
@@ -362,8 +379,29 @@ class RTCDatasetEvaluator:
 
         return
 
-    def compare_actions(self, rtc_actions: Tensor, no_rtc_actions: Tensor, inference_delay: int, prev_chunk_left_over: Tensor, execution_horizon: int) -> dict:
+    def plot_waypoints(self, chunk, start_from: int = 0, color: str | None = None, label: str | None = None):
+        for j in range(chunk.shape[-1]):
+            self.axs[j].plot(
+                np.arange(start_from, start_from + chunk.shape[0]),
+                chunk[:, j],
+                color=color,
+                label=label,
+            )
+            self.axs[j].set_ylabel("Joint angle", fontsize=14)
+            self.axs[j].grid()
+            plt.tick_params(labelsize=14)
+            self.axs[j].legend(loc="upper right", fontsize=14)
+            if j == 2:
+                self.axs[j].set_xlabel("Step #", fontsize=16)
 
+    def compare_actions(
+        self,
+        rtc_actions: Tensor,
+        no_rtc_actions: Tensor,
+        inference_delay: int,
+        prev_chunk_left_over: Tensor,
+        execution_horizon: int,
+    ) -> dict:
         if prev_chunk_left_over is None:
             return None
 