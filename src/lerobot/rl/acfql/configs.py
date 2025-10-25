69225eff51c4f5603f845c7275c7b81cc6cb2ad2
Juan Pizarro <jpizarrom@gmail.com>
2025-10-25 10:49:34 +0200

    save replay buffers default to true

--- src/lerobot/rl/acfql/configs.py
+++ src/lerobot/rl/acfql/configs.py
@@ -7,5 +7,5 @@ from lerobot.configs.train import TrainRLServerPipelineConfig
 @dataclass(kw_only=True)
 class ACFQLTrainRLServerPipelineConfig(TrainRLServerPipelineConfig):
     online_dataset: DatasetConfig | None = None
-    save_offline_replay_buffer_on_checkpoint: bool = False
-    save_replay_buffer_on_checkpoint: bool = False
+    save_offline_replay_buffer_on_checkpoint: bool = True
+    save_replay_buffer_on_checkpoint: bool = True
