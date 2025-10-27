50533f619037dc958d4333fc71940f35dac30494
CarolinePascal <caroline8.pascal@gmail.com>
2025-10-27 14:18:57 +0100

    chore(touchlab imports): adding imports for touchlab sensors

--- src/lerobot/__init__.py
+++ src/lerobot/__init__.py
@@ -178,6 +178,7 @@ available_cameras = [
 # lists all available microphones from `lerobot/microphones`
 available_microphones = [
     "portaudio",
+    "touchlab",
 ]
 
 # lists all available motors from `lerobot/motors`
