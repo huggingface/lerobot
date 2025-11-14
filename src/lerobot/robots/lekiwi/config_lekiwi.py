b833066fa91dd0e43dbcf01330f0bfd7a3c5901f
david <244252585@qq.com>
2025-11-14 17:23:30 +0800

    lewiki 2 camera

--- src/lerobot/robots/lekiwi/config_lekiwi.py
+++ src/lerobot/robots/lekiwi/config_lekiwi.py
@@ -23,10 +23,10 @@ from ..config import RobotConfig
 def lekiwi_cameras_config() -> dict[str, CameraConfig]:
     return {
         "front": OpenCVCameraConfig(
-            index_or_path="/dev/video0", fps=30, width=640, height=480, rotation=Cv2Rotation.ROTATE_180
+            index_or_path="/dev/video0", fps=30, width=640, height=480, rotation=Cv2Rotation.NO_ROTATION
         ),
         "wrist": OpenCVCameraConfig(
-            index_or_path="/dev/video1", fps=30, width=480, height=640, rotation=Cv2Rotation.ROTATE_90
+            index_or_path="/dev/video2", fps=30, width=480, height=640, rotation=Cv2Rotation.NO_ROTATION
         ),
     }
 
