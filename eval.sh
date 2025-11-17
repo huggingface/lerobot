b16bc5f1ff5dee68259c4046d58146eea56610f2
Jade Choghari <chogharijade@gmail.com>
2025-11-17 18:29:28 +0100

    new changes

--- eval.sh
+++ eval.sh
@@ -1,5 +1,5 @@
 lerobot-eval \
-  --policy.path="/raid/jade/models/xvla-libero-og_migrated" \
+  --policy.path="/raid/jade/models/xvla-libero-new_migrated" \
   --env.type=libero \
   --env.task=libero_spatial \
   --env.action_type=abs \
