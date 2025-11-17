80d83c3a49d36a0cf68e8bc1cc71c8ac9abefacc
croissant <pepijnkooijmans@outlook.com>
2025-11-17 10:46:22 +0100

    cam res

--- loop_datasets.py
+++ loop_datasets.py
@@ -2,7 +2,9 @@ from huggingface_hub import HfApi, list_datasets
 
 api = HfApi()
 datasets = list_datasets(author="lerobot-data-collection")
+print('"[', end="")
+i=0
 for dataset in datasets:
-    if "two-folds-dataset" in dataset.id:
-        print("'" + dataset.id + "',", end=" ")
-print("\n")
+    if "three-folds-dataset" in dataset.id:
+        print("'" + dataset.id + "',", end="")
+print(']"',)
