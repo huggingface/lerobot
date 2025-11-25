abaf870e00d478df0b48680a559d1282ab51e6d5
Jade Choghari <chogharijade@gmail.com>
2025-11-25 14:43:46 +0100

    remove .sh file

--- eval.sh
+++
@@ -1,9 +0,0 @@
-lerobot-eval \
-  --policy.path="/raid/jade/models/xvla-libero-new_migrated2" \
-  --env.type=libero \
-  --env.task=libero_spatial \
-  --env.control_mode=absolute \
-  --eval.batch_size=1 \
-  --eval.n_episodes=1 \
-  --env.episode_length=800 \
-  --seed=142
