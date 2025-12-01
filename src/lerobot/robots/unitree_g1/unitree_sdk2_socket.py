23696650931bcf352a8daa329b3ab273aae1bd18
Martino Russi <nopyeps@gmail.com>
2025-12-01 15:47:41 +0100

    add licence header

--- src/lerobot/robots/unitree_g1/unitree_sdk2_socket.py
+++ src/lerobot/robots/unitree_g1/unitree_sdk2_socket.py
@@ -1,12 +1,18 @@
-"""
-ZMQ socket wrapper that mimics the Unitree SDK Channel interface.
+#!/usr/bin/env python
 
-This module provides a drop-in replacement for the Unitree SDK's DDS-based
-ChannelPublisher and ChannelSubscriber, using ZMQ sockets instead. This allows
-remote communication with the robot over WiFi via the robot_server bridge.
-
-Uses JSON for secure serialization instead of pickle.
-"""
+# Copyright 2025 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import base64
 import json
