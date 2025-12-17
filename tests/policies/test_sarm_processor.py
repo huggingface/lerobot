ce74b13c9d67a357b79a00df5d7a590aac82a38e
Pepijn <pepijn@huggingface.co>
2025-12-17 18:14:35 +0100

    run only when we have faker

--- tests/policies/test_sarm_processor.py
+++ tests/policies/test_sarm_processor.py
@@ -14,6 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import pytest
+
+pytest.importorskip("faker")
+
 from unittest.mock import MagicMock, patch
 
 import numpy as np
