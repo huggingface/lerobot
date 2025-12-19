289e9ea368e59efda865e3c6bdb2e169d50e0592
Coded190 <moorechristian15@yahoo.com>
2025-12-19 11:36:58 -1000

    updated teleoperate.py to use the ids, ports, ips

--- examples/lekiwi/teleoperate.py
+++ examples/lekiwi/teleoperate.py
@@ -27,8 +27,8 @@ FPS = 30
 
 def main():
     # Create the robot and teleoperator configurations
-    robot_config = LeKiwiClientConfig(remote_ip="172.18.134.136", id="my_lekiwi")
-    teleop_arm_config = SO100LeaderConfig(port="/dev/tty.usbmodem585A0077581", id="my_awesome_leader_arm")
+    robot_config = LeKiwiClientConfig(remote_ip="192.168.1.64", id="cirp_follower_black")
+    teleop_arm_config = SO100LeaderConfig(port="/dev/tty.usbmodem5A7C1168801", id="cirp_leader_blue")
     keyboard_config = KeyboardTeleopConfig(id="my_laptop_keyboard")
 
     # Initialize the robot and teleoperator
