2ffcb803e09f1c6108370abb89a7831393e9ae8d
Pepijn <pepijn@huggingface.co>
2026-01-28 15:15:34 +0100

    pre-commit

--- src/lerobot/motors/damiao/damiao.py
+++ src/lerobot/motors/damiao/damiao.py
@@ -28,6 +28,7 @@ from lerobot.utils.import_utils import _can_available
 if TYPE_CHECKING or _can_available:
     import can
 else:
+
     class can:  # noqa: N801
         Message = object
         interface = None
