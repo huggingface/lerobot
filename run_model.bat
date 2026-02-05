6a0ef19f605aac699b90333484794137d5c6245e
Flavien Deslandres <flavien.deslandres@smpltd.co.uk>
2026-02-05 13:52:47 +0100

    update

--- run_model.bat
+++ run_model.bat
@@ -3,7 +3,7 @@ REM Simple script to run the trained model on the robot
 REM This handles dataset cleanup automatically
 
 echo Cleaning up previous test dataset...
-rmdir /s /q "C:\Users\picip\.cache\huggingface\lerobot\Zarax\eval_test" 2>nul
+rmdir /s /q "C:\Users\picip\.cache\huggingface\lerobot\Zarax\eval_test" 2>NUL
 
 echo Starting robot with trained model...
 lerobot-record --config_path ./config/eval/zarax_eval_simple.yaml
