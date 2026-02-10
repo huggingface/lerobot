435f12f6e485f87585d6aac33a98ae60030c1a56
Jade Choghari <chogharijade@gmail.com>
2026-02-10 13:07:40 +0100

    pre-commit

--- src/lerobot/processor/env_processor.py
+++ src/lerobot/processor/env_processor.py
@@ -17,7 +17,7 @@ from dataclasses import dataclass
 
 import torch
 
-from lerobot.configs.types import PipelineFeatureType, PolicyFeature, FeatureType
+from lerobot.configs.types import FeatureType, PipelineFeatureType, PolicyFeature
 from lerobot.utils.constants import OBS_IMAGES, OBS_PREFIX, OBS_STATE, OBS_STR
 
 from .pipeline import ObservationProcessorStep, ProcessorStepRegistry
