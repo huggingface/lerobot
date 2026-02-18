7285f5b7163fdd5f74cf55627f20dc1a892a7bc4
Jeremiah Coholich <jcoholich@gatech.edu>
2026-02-17 19:04:57 -0500

    dataset conversion script

--- external_utils/convert_riplh5_lerobot.py
+++ external_utils/convert_riplh5_lerobot.py
@@ -2,12 +2,14 @@ import h5py
 import numpy as np
 from pathlib import Path
 from lerobot.datasets.lerobot_dataset import LeRobotDataset
+from tqdm import tqdm
+import math
 
 # Configuration
-REPO_ID = "lerobot/my_custom_dataset"
-DATASET_NAME = "my_custom_dataset"
-ORIG_DATASET_PATH = Path("/home/razarcon/datasets/test_lerobot_conversion")
-FPS = 5
+REPO_ID = "lerobot/eve_blocks"
+DATASET_NAME = "eve_blocks"
+ORIG_DATASET_PATH = Path("/home/jeremiah/openteach/extracted_data/eve_blocks/h5_files")
+FPS = 20
 ROOT_DIR = Path(f"data/{DATASET_NAME}")  # Where the dataset will be created locally
 
 # Define your features match your HDF5 content
@@ -23,7 +25,7 @@ FEATURES = {
         "shape": (8,),
         "names": ["joint1", "joint2", "joint3", "joint4", "joint5", "joint6", "joint7", "gripper_pos"],
     },
-    "observation.images.camera_external": {
+    "observation.images.camera_front": {
         "dtype": "video",
         "shape": (360, 640, 3), # (H, W, C)
         "names": ["height", "width", "channels"],
@@ -49,43 +51,50 @@ def main():
         root=ROOT_DIR,
         use_videos=True, # Encode images to MP4
     )
-    
+
     # 2. Iterate over all HDF5 files in the directory
     h5_files = sorted(list(ORIG_DATASET_PATH.glob("*.h5")))
     print(f"Found {len(h5_files)} episodes in {ORIG_DATASET_PATH}")
-    
-    for h5_path in h5_files:
+
+    for h5_path in tqdm(h5_files):
         print(f"Processing {h5_path.name}...")
         with h5py.File(h5_path, "r") as f:
             # Extract hdf5 data
+
             actions = np.concatenate([f['arm_action'], np.expand_dims(f['gripper_action'],axis=1)], axis=1)
             observation_states = np.concatenate([f['joint_pos'], np.expand_dims(f['gripper_state'], axis=1)], axis=1)
-            camera_external_imgs = f['rgb_frames'][:, 0, :]
+            camera_side_imgs = f['rgb_frames'][:, 0, :]
             camera_wrist_imgs = f['rgb_frames'][:, 1, :]
-            camera_side_imgs = f['rgb_frames'][:, 2, :]
-            
+            camera_front_imgs = f['rgb_frames'][:, 2, :]
+
             num_frames = observation_states.shape[0]
-            
+
             # 3. Iterate over frames
             for i in range(num_frames):
                 frame = {
-                    "action": actions[i], 
+                    "action": actions[i],
                     "observation.state": observation_states[i],
-                    "observation.images.camera_external": camera_external_imgs[i],
-                    "observation.images.camera_side": camera_wrist_imgs[i],
-                    "observation.images.camera_wrist": camera_side_imgs[i],
-                    "task": "Demo Task",
+                    "observation.images.camera_front": camera_front_imgs[i],
+                    "observation.images.camera_wrist": camera_wrist_imgs[i],
+                    "observation.images.camera_side": camera_side_imgs[i],
+                    "task": get_task_instructions(h5_path.name),
                 }
-                
+
                 # 4. Add frame to buffer
                 dataset.add_frame(frame)
-            
+
             # 5. Save the completed episode (one file = one episode)
             dataset.save_episode()
 
     # 6. Finalize (closes writers and saves metadata)
     dataset.finalize()
     print(f"Dataset created at: {dataset.root}")
+
+
+def get_task_instructions(fname):
+    color = fname.split('_')[2]
+    return f"pick up the {color} block"
+
 
 if __name__ == "__main__":
     main()
