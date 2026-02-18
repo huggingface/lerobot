c7f354644f3c1c32980a42e9874f4c1c7c241289
Jeremiah Coholich <jmcoholich@gmail.com>
2026-02-18 12:28:11 -0500

    update skynet scripts;

---
+++ pi05_training.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --job-name=pi05_training
+#SBATCH -p kira-lab
+#SBATCH -A kira-lab
+#SBATCH -G a40:1
+#SBATCH -c 10
+#SBATCH --mem=16G
+#SBATCH --qos=long
+
+JOB_NAME=$1
+OUTDIR=./outputs/$JOB_NAME
+
+echo "Job name: $JOB_NAME"
+echo "Output dir: $OUTDIR"
+
+source /coc/testnvme/$USER/.bashrc
+conda activate lerobot
+
+python src/lerobot/scripts/lerobot_train.py\
+    --dataset.repo_id=eve_blocks \
+    --dataset.root='/coc/testnvme/jcoholich3/lerobot_data/eve_blocks' \
+    --policy.type=pi05 \
+    --output_dir=$OUTDIR \
+    --job_name=$JOB_NAME \
+    --policy.repo_id=your_repo_id \
+    --policy.pretrained_path=lerobot/pi05_base \
+    --policy.compile_model=false \
+    --policy.gradient_checkpointing=true \
+    --wandb.enable=true \
+    --policy.dtype=bfloat16 \
+    --policy.freeze_vision_encoder=false \
+    --policy.train_expert_only=false \
+    --steps=3000 \
+    --policy.device=cuda \
+    --batch_size=32 \
+    --log_freq=5
+
