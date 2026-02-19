e3b0b67d10de30a41653fd8bf4532f5ef87dfe76
Jade Choghari <chogharijade@gmail.com>
2026-02-19 12:25:07 +0000

    revert groot

--- src/lerobot/policies/__init__.py
+++ src/lerobot/policies/__init__.py
@@ -15,7 +15,7 @@
 from .act.configuration_act import ACTConfig as ACTConfig
 from .diffusion.configuration_diffusion import DiffusionConfig as DiffusionConfig
 
-# from .groot.configuration_groot import GrootConfig as GrootConfig
+from .groot.configuration_groot import GrootConfig as GrootConfig
 from .pi0.configuration_pi0 import PI0Config as PI0Config
 from .pi0_fast.configuration_pi0_fast import PI0FastConfig as PI0FastConfig
 from .pi05.configuration_pi05 import PI05Config as PI05Config
@@ -36,7 +36,7 @@ __all__ = [
     "SARMConfig",
     "TDMPCConfig",
     "VQBeTConfig",
-    # "GrootConfig",
+    "GrootConfig",
     "XVLAConfig",
     "WallXConfig",
 ]
