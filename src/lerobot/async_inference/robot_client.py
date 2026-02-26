4ec31b3ed9534bd91f4c8ca152cadde6624b98e3
Damien LaRocque <phicoltan@gmail.com>
2026-02-26 13:56:41 +0100

    Fix import error in Robot Client

--- src/lerobot/async_inference/robot_client.py
+++ src/lerobot/async_inference/robot_client.py
@@ -58,7 +58,7 @@ from lerobot.transport import (
     services_pb2_grpc,  # type: ignore
 )
 from lerobot.transport.utils import grpc_channel_options, send_bytes_in_chunks
-from lerobot.utils.import_utils import register_third_party_devices
+from lerobot.utils.import_utils import register_third_party_plugins
 
 from .configs import RobotClientConfig
 from .helpers import (
@@ -508,5 +508,5 @@ def async_client(cfg: RobotClientConfig):
 
 
 if __name__ == "__main__":
-    register_third_party_devices()
+    register_third_party_plugins()
     async_client()  # run the client
