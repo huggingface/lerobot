92f08933ec63450400a0e12de69659e13a32ca16
CarolinePascal <caroline8.pascal@gmail.com>
2026-03-11 11:02:07 +0100

    fix(features): allowing for sequence of shape (1,) when a names list is provided

--- src/lerobot/datasets/utils.py
+++ src/lerobot/datasets/utils.py
@@ -578,7 +578,7 @@ def get_hf_features_from_features(features: dict) -> datasets.Features:
             continue
         elif ft["dtype"] == "image":
             hf_features[key] = datasets.Image()
-        elif ft["shape"] == (1,):
+        elif ft["shape"] == (1,) and ft["names"] is None:
             hf_features[key] = datasets.Value(dtype=ft["dtype"])
         elif len(ft["shape"]) == 1:
             hf_features[key] = datasets.Sequence(
