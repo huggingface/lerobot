5a30921627472be2d98bae27d3f00c20fb4a4a1f
wy-zong <wuc120@gmail.com>
2026-04-14 19:54:29 +0800

    Refactor import statements for BiSOFollower and BiSOLeader
    Signed-off-by: wy-zong <wuc120@gmail.com>

--- examples/hil/hil_data_collection.py
+++ examples/hil/hil_data_collection.py
@@ -137,10 +137,10 @@ from lerobot.processor import (
 )
 from lerobot.robots import Robot, RobotConfig, make_robot_from_config
 from lerobot.robots.bi_openarm_follower import BiOpenArmFollowerConfig
-from lerobot.robots.bi_so_follower.config_bi_so_follower import BiSOFollowerConfig  # noqa: F401
+from lerobot.robots.bi_so_follower import BiSOFollowerConfig  # noqa: F401
 from lerobot.robots.so_follower import SOFollowerRobotConfig  # noqa: F401
 from lerobot.teleoperators import Teleoperator, TeleoperatorConfig, make_teleoperator_from_config
-from lerobot.teleoperators.bi_so_leader.config_bi_so_leader import BiSOLeaderConfig  # noqa: F401
+from lerobot.teleoperators.bi_so_leader import BiSOLeaderConfig  # noqa: F401
 from lerobot.teleoperators.openarm_mini import OpenArmMiniConfig  # noqa: F401
 from lerobot.teleoperators.so_leader import SOLeaderTeleopConfig  # noqa: F401
 from lerobot.utils import get_safe_torch_device
