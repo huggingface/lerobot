b858ba1b6ce4f8be1849c24a61f5d54c917f45a3
hq-fang <71356829+hq-fang@users.noreply.github.com>
2026-05-19 21:57:45 +0000

    simplify molmoact2 package imports

--- src/lerobot/policies/molmoact2/__init__.py
+++ src/lerobot/policies/molmoact2/__init__.py
@@ -15,17 +15,7 @@
 # limitations under the License.
 
 from .configuration_molmoact2 import MolmoAct2Config
+from .modeling_molmoact2 import MolmoAct2Policy
+from .processor_molmoact2 import make_molmoact2_pre_post_processors
 
 __all__ = ["MolmoAct2Config", "MolmoAct2Policy", "make_molmoact2_pre_post_processors"]
-
-
-def __getattr__(name):
-    if name == "MolmoAct2Policy":
-        from .modeling_molmoact2 import MolmoAct2Policy
-
-        return MolmoAct2Policy
-    if name == "make_molmoact2_pre_post_processors":
-        from .processor_molmoact2 import make_molmoact2_pre_post_processors
-
-        return make_molmoact2_pre_post_processors
-    raise AttributeError(name)
