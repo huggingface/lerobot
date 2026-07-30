0d5ac60dd39b0655840b5ee6a59ec2250ac04082
Pepijn <pepijn@huggingface.co>
2026-07-30 10:45:38 +0200

    refactor(hy_vla): remove modeling package marker

--- src/lerobot/policies/hy_vla/modeling/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Bundled model sources from Tencent Hy-VLA.
-
-Source: https://github.com/Tencent-Hunyuan/Hy-Embodied-0.5-VLA
-Revision: 8ba4c8cbdf42a4bcf0a19be4bd2841405dfe15e9
-
-Imports are intentionally lazy: loading ``lerobot.policies`` must not import
-Transformers.
-"""
