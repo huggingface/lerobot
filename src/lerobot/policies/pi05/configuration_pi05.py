31d16c8fd87c9069b75adbad08fad7d938fac2bc
Pepijn <pepijn@huggingface.co>
2026-08-05 12:51:37 +0200

    chore(pi05): remove duplicate tokenizer config field

--- src/lerobot/policies/pi05/configuration_pi05.py
+++ src/lerobot/policies/pi05/configuration_pi05.py
@@ -103,8 +103,6 @@ class PI05Config(PreTrainedConfig):
     scheduler_decay_steps: int = 30_000
     scheduler_decay_lr: float = 2.5e-6
 
-    tokenizer_max_length: int = 200  # see openpi `__post_init__`
-
     def __post_init__(self):
         super().__post_init__()
 
